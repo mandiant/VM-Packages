1cbc7e29e23f8b433f18db59b967aaccf37e273d
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2021-12-16 19:07:00 +0100

    Break capa to test testsuite
    This is a test. This commit should make the GH action fail.

--- packages/capa.vm/tools/chocolateyinstall.ps1
+++ packages/capa.vm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ $toolName = 'capa'
 $category = 'Utilities'
 
 $zipUrl = "https://github.com/mandiant/capa/releases/download/v1.6.3/capa-v1.6.3-windows.zip"
-$zipSha256 = "00e8d32941b3a1a58a164efc38826099fd70856156762647c4bbd9e946e41606"
+$zipSha256 = "11e8d32941b3a1a58a164efc38826099fd70856156762647c4bbd9e946e41606"
 
 VM-Install-From-Zip $toolName $category $zipUrl $zipSha256 -consoleApp $true
 
