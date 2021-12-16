4c96d89b7b6f1c4c2ceffdf707b1b84ae07a4b2b
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2021-12-16 18:51:10 +0100

    Add ComparePlus plugin for Notepad++
    I like to use this Notepad++ plugin to compare files.

---
+++ packages/notepadpp.plugin.compare.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = "ComparePlugin"
+  $pluginsDir= Join-Path ${Env:ProgramFiles} "Notepad++\plugins" -Resolve
+  $toolDir = New-Item (Join-Path $pluginsDir $toolName) -itemtype directory
+  VM-Assert-Path $toolDir
+
+  $zipUrl= "https://github.com/pnedev/compare-plugin/releases/download/v2.0.1/ComparePlugin_v2.0.1_x86.zip"
+  $zipSha256 ="07972c1c7e3012a46ac6ef133a6500ca851bddc9c83471df2f118519a0241ed5"
+  $zipUrl_64 = "https://github.com/pnedev/compare-plugin/releases/download/v2.0.1/ComparePlugin_v2.0.1_X64.zip"
+  $zipSha256_64 ="77dedf98ea2280528d726c0053db2001e90da7588e14ee01a98933f121bb15cb"
+
+  # Download and unzip
+  $packageArgs = @{
+    packageName    = ${Env:ChocolateyPackageName}
+    unzipLocation  = $toolDir
+    url            = $zipUrl
+    checksum       = $zipSha256
+    checksumType   = 'sha256'
+    url64bit       = $zipUrl_64
+    checksum64     = $zipSha256_64
+  }
+  Install-ChocolateyZipPackage @packageArgs
+  VM-Assert-Path (Join-Path $toolDir "$toolName.dll")
+} catch {
+  VM-Write-Log-Exception $_
+}
