e2bd4fca9dda251d0cfc87fe09179eff1095469c
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2021-12-20 12:33:07 +0100

    Use a fix commit to avoid failures
    Use a fix commit instead of master branch to avoid that a future update
    changes the hash.
    
    I have used the project/commit date instead of today's date.

--- packages/peid.vm/tools/chocolateyinstall.ps1
+++ packages/peid.vm/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'peid'
 $category = 'Utilities'
 
-$zipUrl = "https://github.com/wolfram77web/app-peid/archive/refs/heads/master.zip"
-$zipSha256 = "8c85ec0a15bb89443a154fac57af2d386af267e2cbc7c7315db0d6a905de9779"
+$zipUrl = "https://codeload.github.com/wolfram77web/app-peid/zip/91b0057697fb143205a6071a4482e7ad1ff37e12"
+$zipSha256 = "04cec2d03338a52f540b88b9fe4dff5922888beb9a3eea4a23308850400c9ee5"
 
 VM-Install-From-Zip $toolName $category $zipUrl $zipSha256 -innerFolder $true
 
