91fbf0bc7f3df41778e23fd79bdbe7f6502fd614
Blaine Stancill <malwaremechanic@gmail.com>
2022-01-25 09:54:12 -0500

    Update processdump.vm with GitHub URLs

--- packages/processdump.vm/tools/chocolateyinstall.ps1
+++ packages/processdump.vm/tools/chocolateyinstall.ps1
@@ -2,46 +2,56 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  # Disable Windows Defender
-  Set-MpPreference -DisableRealtimeMonitoring $true
-  New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force
-  New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
-  Uninstall-WindowsFeature -Name Windows-Defender
   $toolName = "pd"
   $category = "Utilities"
 
-  $toolDirParent = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-  $toolDir = Join-Path $toolDirParent $toolName
+  $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} 'Process-Dump'
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
-  $zipUrl = "http://www.split-code.com/files/pd_v2_1.zip"
-  $zipSha256 = "2060f6713c082e78b2c18e6cb4e195602296af7ff9b096931f5f9f70a145b487"
 
-  # Remove files from previous zips for upgrade
-  VM-Remove-PreviousZipPackage ${Env:chocolateyPackageFolder}
+  $url = 'https://github.com/glmcdona/Process-Dump/releases/download/v2.1.1/pd32.exe'
+  $checksum = 'c5d3535d33797a3b62916f5ea16b8710a0f5a7ce79ca4b2920eab245d99980d1'
 
+  $executablePath = Join-Path $toolDir ($toolName + "32.exe")
   $packageArgs = @{
-    packageName   = ${Env:ChocolateyPackageName}
-    unzipLocation = $toolDir
-    url           = $zipUrl
-    checksum       = $zipSha256
-    checksumType   = 'sha256'
+    packageName = ${Env:ChocolateyPackageName}
+    url = $url
+    checksum = $checksum
+    checksumType = "sha256"
+    fileFullPath = $executablePath
+    forceDownload = $true
   }
-  Install-ChocolateyZipPackage @packageArgs
+  Get-ChocolateyWebFile @packageArgs
+  VM-Assert-Path $executablePath
 
-  $executablePath = Join-Path $toolDir ($toolName + "32.exe") -Resolve
   $executableCmd  = Join-Path ${Env:WinDir} "system32\cmd.exe" -Resolve
   $executableDir  = Join-Path ${Env:UserProfile} "Desktop" -Resolve
-  $executableArgs = "/K `"cd ${executableDir} && $executablePath -h`""
+  $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`" --help`""
   $shortcut = Join-Path $shortcutDir ($toolName + "32.lnk")
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir -RunAsAdmin
   VM-Assert-Path $shortcut
+  Install-BinFile -Name ($toolName + "32") -Path $executablePath
 
   if (Get-OSArchitectureWidth -Compare 64) {
-    $executablePath64 = Join-Path $toolDir ($toolName + "64.exe") -Resolve
-    $executableArgs64 = "/K `"cd ${executableDir} && $executablePath64 -h`""
-    $shortcut64 = Join-Path $shortcutDir ($toolName + "64.lnk")
-    Install-ChocolateyShortcut -shortcutFilePath $shortcut64 -targetPath $executableCmd -Arguments $executableArgs64 -WorkingDirectory $executableDir -RunAsAdmin
-    VM-Assert-Path $shortcut64
+    $url = 'https://github.com/glmcdona/Process-Dump/releases/download/v2.1.1/pd64.exe'
+    $checksum = 'f2c2d46331ddf2a4982ada7f3f2ea3a0946b99204d172b68d7bd6301eac5bb95'
+
+    $executablePath = Join-Path $toolDir ($toolName + "64.exe")
+    $packageArgs = @{
+      packageName = ${Env:ChocolateyPackageName}
+      url64bit = $url
+      checksum64 = $checksum
+      checksumType = "sha256"
+      fileFullPath = $executablePath
+      forceDownload = $true
+    }
+    Get-ChocolateyWebFile @packageArgs
+    VM-Assert-Path $executablePath
+
+    $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`" --help`""
+    $shortcut = Join-Path $shortcutDir ($toolName + "64.lnk")
+    Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir -RunAsAdmin
+    VM-Assert-Path $shortcut
+    Install-BinFile -Name ($toolName + "64") -Path $executablePath
   }
 } catch {
   VM-Write-Log-Exception $_
--- packages/processdump.vm/tools/chocolateyuninstall.ps1
+++ packages/processdump.vm/tools/chocolateyuninstall.ps1
@@ -6,3 +6,9 @@ $category = "Utilities"
 
 VM-Remove-Tool-Shortcut ($toolName + "32") $category
 VM-Remove-Tool-Shortcut ($toolName + "64") $category
+
+Uninstall-BinFile -Name ($toolName + "32")
+Uninstall-BinFile -Name ($toolName + "64")
+
+$toolDir = Join-Path ${Env:RAW_TOOLS_DIR} "Process-Dump"
+Remove-Item $toolDir -Recurse -Force -ea 0 | Out-Null
