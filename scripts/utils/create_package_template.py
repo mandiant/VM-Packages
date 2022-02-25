0dfd5a450c221eaf464555e673073d3eae436916
Moritz Raabe <moritz.raabe@mandiant.com>
2022-02-25 13:33:57 +0100

    isort and black code format

--- scripts/utils/create_package_template.py
+++ scripts/utils/create_package_template.py
@@ -1,9 +1,8 @@
-import argparse
-import logging
 import os
 import sys
+import logging
+import argparse
 import textwrap
-
 
 # Set up logger
 logging.basicConfig(
@@ -37,7 +36,7 @@ CATEGORIES = (
     "Text Editors",
     "Utilities",
     "VB",
-    "Web Application"
+    "Web Application",
 )
 
 UNINSTALL_TEMPLATE_NAME = "chocolateyuninstall.ps1"
@@ -48,7 +47,7 @@ Needs the following format strings:
     pkg_name="...", version="...", authors="...", description="..."
 """
 NUSPEC_TEMPLATE_NAME = "{}.vm.nuspec"
-NUSPEC_TEMPLATE = r'''<?xml version="1.0" encoding="utf-8"?>
+NUSPEC_TEMPLATE = r"""<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd">
   <metadata>
     <id>{pkg_name}.vm</id>
@@ -60,13 +59,13 @@ NUSPEC_TEMPLATE = r'''<?xml version="1.0" encoding="utf-8"?>
     </dependencies>
   </metadata>
 </package>
-'''
+"""
 
 """
 Needs the following format strings:
     pkg_name="...", version="...", authors="...", description="...", dependency="..."
 """
-NUSPEC_TEMPLATE_METAPACKAGE = r'''<?xml version="1.0" encoding="utf-8"?>
+NUSPEC_TEMPLATE_METAPACKAGE = r"""<?xml version="1.0" encoding="utf-8"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2015/06/nuspec.xsd">
   <metadata>
     <id>{pkg_name}.vm</id>
@@ -79,13 +78,13 @@ NUSPEC_TEMPLATE_METAPACKAGE = r'''<?xml version="1.0" encoding="utf-8"?>
     </dependencies>
   </metadata>
 </package>
-'''
+"""
 
 """
 Needs the following format strings:
     tool_name="...", category="...", zip_url="...", zip_hash="..."
 """
-ZIP_EXE_TEMPLATE = r'''$ErrorActionPreference = 'Stop'
+ZIP_EXE_TEMPLATE = r"""$ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = '{tool_name}'
@@ -95,14 +94,14 @@ $zipUrl = '{zip_url}'
 $zipSha256 = '{zip_hash}'
 
 VM-Install-From-Zip $toolName $category $zipUrl $zipSha256
-'''
+"""
 
 
 """
 Needs the following format strings:
     tool_name="...", category="...", zip_url="...", zip_hash="..."
 """
-GITHUB_REPO_TEMPLATE = r'''$ErrorActionPreference = 'Stop'
+GITHUB_REPO_TEMPLATE = r"""$ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = '{tool_name}'
@@ -112,14 +111,14 @@ $zipUrl = '{zip_url}'
 $zipSha256 = '{zip_hash}'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
-'''
+"""
 
 
 """
 Needs the following format strings:
     tool_name="...", category="...", shim_path="..."
 """
-METAPACKAGE_TEMPLATE = r'''$ErrorActionPreference = 'Stop'
+METAPACKAGE_TEMPLATE = r"""$ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {{
@@ -135,183 +134,184 @@ try {{
 }} catch {{
   VM-Write-Log-Exception $_
 }}
-'''
+"""
 
 
 """
 Needs the following format strings:
     tool_name="...", category="..."
 """
-GENERIC_UNINSTALL_TEMPLATE = r'''$ErrorActionPreference = 'Continue'
+GENERIC_UNINSTALL_TEMPLATE = r"""$ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = '{tool_name}'
 $category = '{category}'
 
 VM-Uninstall $toolName $category
-'''
+"""
 
 """
 Needs the following format strings:
     tool_name="...", category="..."
 """
-METAPACKAGE_UNINSTALL_TEMPLATE = r'''$ErrorActionPreference = 'Continue'
+METAPACKAGE_UNINSTALL_TEMPLATE = r"""$ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = '{tool_name}'
 $category = '{category}'
 
 VM-Remove-Tool-Shortcut $toolName $category
-'''
+"""
 
 
-def create_git_repo_template(
-  packages_path,
-  **kwargs
-  ):
-  create_template(
-    GITHUB_REPO_TEMPLATE,
-    packages_path=packages_path,
-    pkg_name=kwargs.get("pkg_name"),
-    version=kwargs.get("version"),
-    authors=kwargs.get("authors"),
-    description=kwargs.get("description"),
-    tool_name=kwargs.get("tool_name"),
-    category=kwargs.get("category"),
-    zip_url=kwargs.get("zip_url"),
-    zip_hash=kwargs.get("zip_hash"),
-  )
+def create_git_repo_template(packages_path, **kwargs):
+    create_template(
+        GITHUB_REPO_TEMPLATE,
+        packages_path=packages_path,
+        pkg_name=kwargs.get("pkg_name"),
+        version=kwargs.get("version"),
+        authors=kwargs.get("authors"),
+        description=kwargs.get("description"),
+        tool_name=kwargs.get("tool_name"),
+        category=kwargs.get("category"),
+        zip_url=kwargs.get("zip_url"),
+        zip_hash=kwargs.get("zip_hash"),
+    )
 
 
-def create_zip_exe_template(
-  packages_path,
-  **kwargs
-  ):
-  create_template(
-    ZIP_EXE_TEMPLATE,
-    packages_path=packages_path,
-    pkg_name=kwargs.get("pkg_name"),
-    version=kwargs.get("version"),
-    authors=kwargs.get("authors"),
-    description=kwargs.get("description"),
-    tool_name=kwargs.get("tool_name"),
-    category=kwargs.get("category"),
-    zip_url=kwargs.get("zip_url"),
-    zip_hash=kwargs.get("zip_hash"),
-  )
+def create_zip_exe_template(packages_path, **kwargs):
+    create_template(
+        ZIP_EXE_TEMPLATE,
+        packages_path=packages_path,
+        pkg_name=kwargs.get("pkg_name"),
+        version=kwargs.get("version"),
+        authors=kwargs.get("authors"),
+        description=kwargs.get("description"),
+        tool_name=kwargs.get("tool_name"),
+        category=kwargs.get("category"),
+        zip_url=kwargs.get("zip_url"),
+        zip_hash=kwargs.get("zip_hash"),
+    )
 
 
-def create_metapackage_template(
-  packages_path,
-  **kwargs
-):
-  create_template(
-    METAPACKAGE_TEMPLATE,
-    nuspec_template=NUSPEC_TEMPLATE_METAPACKAGE,
-    uninstall_template=METAPACKAGE_UNINSTALL_TEMPLATE,
-    packages_path=packages_path,
-    pkg_name=kwargs.get("pkg_name"),
-    version=kwargs.get("version"),
-    authors=kwargs.get("authors"),
-    description=kwargs.get("description"),
-    dependency=kwargs.get("dependency"),
-    tool_name=kwargs.get("tool_name"),
-    category=kwargs.get("category"),
-    shim_path=kwargs.get("shim_path"),
-  )
+def create_metapackage_template(packages_path, **kwargs):
+    create_template(
+        METAPACKAGE_TEMPLATE,
+        nuspec_template=NUSPEC_TEMPLATE_METAPACKAGE,
+        uninstall_template=METAPACKAGE_UNINSTALL_TEMPLATE,
+        packages_path=packages_path,
+        pkg_name=kwargs.get("pkg_name"),
+        version=kwargs.get("version"),
+        authors=kwargs.get("authors"),
+        description=kwargs.get("description"),
+        dependency=kwargs.get("dependency"),
+        tool_name=kwargs.get("tool_name"),
+        category=kwargs.get("category"),
+        shim_path=kwargs.get("shim_path"),
+    )
 
 
 def create_template(
-  template="",
-  nuspec_template=NUSPEC_TEMPLATE,
-  uninstall_template=GENERIC_UNINSTALL_TEMPLATE,
-  packages_path="",
-  pkg_name="",
-  version="",
-  authors="",
-  description="",
-  tool_name="",
-  category="",
-  zip_url="",
-  zip_hash="",
-  shim_path="",
-  dependency="",
-  ):
-  pkg_path = os.path.join(packages_path, f"{pkg_name}.vm")
-  try:
-    os.makedirs(pkg_path)
-  except:
-    logger.debug(f"Directory already exists: {pkg_path}")
+    template="",
+    nuspec_template=NUSPEC_TEMPLATE,
+    uninstall_template=GENERIC_UNINSTALL_TEMPLATE,
+    packages_path="",
+    pkg_name="",
+    version="",
+    authors="",
+    description="",
+    tool_name="",
+    category="",
+    zip_url="",
+    zip_hash="",
+    shim_path="",
+    dependency="",
+):
+    pkg_path = os.path.join(packages_path, f"{pkg_name}.vm")
+    try:
+        os.makedirs(pkg_path)
+    except:
+        logger.debug(f"Directory already exists: {pkg_path}")
 
-  tools_path = os.path.join(pkg_path, "tools")
-  try:
-    os.makedirs(tools_path)
-  except:
-    logger.debug(f"Directory already exists: {tools_path}")
+    tools_path = os.path.join(pkg_path, "tools")
+    try:
+        os.makedirs(tools_path)
+    except:
+        logger.debug(f"Directory already exists: {tools_path}")
 
-  with open(os.path.join(pkg_path, NUSPEC_TEMPLATE_NAME.format(pkg_name)), 'w') as f:
-    f.write(nuspec_template.format(pkg_name=pkg_name,
-                                   version=version or "0.0.0",
-                                   authors=authors,
-                                   description=description,
-                                   dependency=dependency))
+    with open(os.path.join(pkg_path, NUSPEC_TEMPLATE_NAME.format(pkg_name)), "w") as f:
+        f.write(
+            nuspec_template.format(
+                pkg_name=pkg_name,
+                version=version or "0.0.0",
+                authors=authors,
+                description=description,
+                dependency=dependency,
+            )
+        )
 
-  with open(os.path.join(tools_path, INSTALL_TEMPLATE_NAME), 'w') as f:
-    f.write(template.format(
-      tool_name=tool_name,
-      category=category,
-      zip_url=zip_url,
-      zip_hash=zip_hash,
-      shim_path=shim_path,
-      ))
+    with open(os.path.join(tools_path, INSTALL_TEMPLATE_NAME), "w") as f:
+        f.write(
+            template.format(
+                tool_name=tool_name,
+                category=category,
+                zip_url=zip_url,
+                zip_hash=zip_hash,
+                shim_path=shim_path,
+            )
+        )
 
-  with open(os.path.join(tools_path, UNINSTALL_TEMPLATE_NAME), 'w') as f:
-    f.write(uninstall_template.format(tool_name=tool_name, category=category))
+    with open(os.path.join(tools_path, UNINSTALL_TEMPLATE_NAME), "w") as f:
+        f.write(uninstall_template.format(tool_name=tool_name, category=category))
 
 
 def get_script_directory():
-  path = os.path.realpath(sys.argv[0])
-  if os.path.isdir(path):
-    return path
-  else:
-    return os.path.dirname(path)
+    path = os.path.realpath(sys.argv[0])
+    if os.path.isdir(path):
+        return path
+    else:
+        return os.path.dirname(path)
 
 
 if __name__ == "__main__":
-  # TODO define required arguments per type
-  # dict[str, Callable]
-  TYPES = {
-    "ZIP_EXE": create_zip_exe_template,
-    "GITHUB_REPO": create_git_repo_template,
-    "METAPACKAGE": create_metapackage_template,
-  }
+    # TODO define required arguments per type
+    # dict[str, Callable]
+    TYPES = {
+        "ZIP_EXE": create_zip_exe_template,
+        "GITHUB_REPO": create_git_repo_template,
+        "METAPACKAGE": create_metapackage_template,
+    }
 
-  epilog = textwrap.dedent(
-    '''
+    epilog = textwrap.dedent(
+        """
     Example usage:
       python %(prog)s --pkg_name <> --version <> --authors <> --tool_name <> --category <>
-    '''
-  )
-  parser = argparse.ArgumentParser(description="A CLI tool to create package templates.", epilog=epilog)
-  parser.add_argument("--pkg_name", type=str, required=True, help="Package name without suffix (i.e., no '.vm' needed)")
-  parser.add_argument("--version", type=str, required=True, help="Tool's version number")
-  parser.add_argument("--authors", type=str, required=True, help="Comma separated list of authors for tool")
-  parser.add_argument("--tool_name", type=str, required=True, help="Name of tool (usually the file name with the '.exe')")
-  parser.add_argument("--category", type=str, choices=CATEGORIES, required=True, help="Category for tool")
-  parser.add_argument("--description", type=str, default="", required=False, help="Description for tool")
-  parser.add_argument("--dependency", type=str, default="", required=False, help="Metapackage dependency")
-  parser.add_argument("--zip_url", type=str, default="", required=False, help="URL to ZIP file")
-  parser.add_argument("--zip_hash", type=str, default="", required=False, help="SHA256 hash of ZIP file")
-  parser.add_argument("--shim_path", type=str, default="", required=False, help="Metapackage shim path")
-  parser.add_argument("--type", type=str, choices=TYPES.keys(), required=True, help="Template type.")
-  args = parser.parse_args()
+    """
+    )
+    parser = argparse.ArgumentParser(description="A CLI tool to create package templates.", epilog=epilog)
+    parser.add_argument(
+        "--pkg_name", type=str, required=True, help="Package name without suffix (i.e., no '.vm' needed)"
+    )
+    parser.add_argument("--version", type=str, required=True, help="Tool's version number")
+    parser.add_argument("--authors", type=str, required=True, help="Comma separated list of authors for tool")
+    parser.add_argument(
+        "--tool_name", type=str, required=True, help="Name of tool (usually the file name with the '.exe')"
+    )
+    parser.add_argument("--category", type=str, choices=CATEGORIES, required=True, help="Category for tool")
+    parser.add_argument("--description", type=str, default="", required=False, help="Description for tool")
+    parser.add_argument("--dependency", type=str, default="", required=False, help="Metapackage dependency")
+    parser.add_argument("--zip_url", type=str, default="", required=False, help="URL to ZIP file")
+    parser.add_argument("--zip_hash", type=str, default="", required=False, help="SHA256 hash of ZIP file")
+    parser.add_argument("--shim_path", type=str, default="", required=False, help="Metapackage shim path")
+    parser.add_argument("--type", type=str, choices=TYPES.keys(), required=True, help="Template type.")
+    args = parser.parse_args()
 
-  root_dir = os.path.dirname(os.path.dirname(get_script_directory()))
-  packages_path = os.path.join(root_dir, 'packages')
+    root_dir = os.path.dirname(os.path.dirname(get_script_directory()))
+    packages_path = os.path.join(root_dir, "packages")
 
-  create_type_template_cb = TYPES.get(args.type)
+    create_type_template_cb = TYPES.get(args.type)
 
-  # remove type before passing to template create function
-  del args.type
+    # remove type before passing to template create function
+    del args.type
 
-  create_type_template_cb(packages_path, **vars(args))
+    create_type_template_cb(packages_path, **vars(args))
