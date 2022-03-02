bfeab5e7a739d93a34c74f8cd21c735b7a306684
Moritz Raabe <moritz.raabe@mandiant.com>
2022-03-02 08:40:49 +0100

    address review feedback

--- packages/apktool.vm/tools/chocolateyinstall.ps1
+++ packages/apktool.vm/tools/chocolateyinstall.ps1
@@ -2,20 +2,19 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  $toolLnk = 'apktool.lnk'
+  $toolName = 'apktool'
   $category = 'Android'
   $shimPath = 'bin\apktool.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
-  $shortcut = Join-Path $shortcutDir $toolLnk
+  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
 
   $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
-  $executableIcon = ""  # no icon
   $executableCmd  = Join-Path ${Env:WinDir} "system32\cmd.exe"
   $executableDir  = Join-Path ${Env:UserProfile} "Desktop"
   $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`""
 
-  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir -IconLocation $executableIcon
+  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir
   VM-Assert-Path $shortcut
 } catch {
   VM-Write-Log-Exception $_
