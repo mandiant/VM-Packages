da295a0fe588066f61f9a78c0031c23c72719881
Moritz <mr-tz@users.noreply.github.com>
2022-03-03 08:35:34 +0100

    add apktool metapackage (#32)
    * add apktool metapackage
    
    * address review feedback

---
+++ packages/apktool.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = 'apktool'
+  $category = 'Android'
+  $shimPath = 'bin\apktool.exe'
+
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+
+  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  $executableCmd  = Join-Path ${Env:WinDir} "system32\cmd.exe"
+  $executableDir  = Join-Path ${Env:UserProfile} "Desktop"
+  $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`""
+
+  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir
+  VM-Assert-Path $shortcut
+} catch {
+  VM-Write-Log-Exception $_
+}
