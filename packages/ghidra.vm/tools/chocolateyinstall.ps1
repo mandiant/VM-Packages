8393be49f96bda6ee2c08997999dd39af103510c
Moritz <mr-tz@users.noreply.github.com>
2022-04-04 11:47:48 +0200

    Apply suggestions from code review
    Co-authored-by: MalwareMechanic <malwaremechanic@gmail.com>

--- packages/ghidra.vm/tools/chocolateyinstall.ps1
+++ packages/ghidra.vm/tools/chocolateyinstall.ps1
@@ -11,8 +11,8 @@ try {
   # get path to ghidra dependency directory
   $toolsDir = $toolsDir -replace ".vm"
   $installDir = Join-Path $toolsDir $versionPath
-  $target = Join-Path $installDir "ghidraRun.bat"
-  $icon = Join-Path $installDir -ChildPath 'support/ghidra.ico'
+  $target = Join-Path $installDir "ghidraRun.bat" -Resolve
+  $icon = Join-Path $installDir "support/ghidra.ico" -Resolve
   Install-BinFile -Name $toolName -Path $target
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
@@ -20,29 +20,29 @@ try {
   $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin -IconLocation $icon
   VM-Assert-Path $shortcut
+
+  # Attempt to set JDK_HOME for Ghidra
+  $openjdkPath = Join-Path ${Env:ProgramFiles} "OpenJDK"
+  if (Test-Path $openjdkPath) {
+    $files = Get-ChildItem -Path $openjdkPath -Filter openjdk* | Sort-Object -Descending
+    if ($files.count -gt 0) {
+        $selectedDir = $files | Select-Object -first 1
+        $openjdkPath = Join-Path $openjdkPath $selectedDir
+        Install-ChocolateyEnvironmentVariable -VariableName "JDK_HOME" -VariableValue $openjdkPath -VariableType 'Machine'
+
+        # Add it do ghidra's config file as well
+        $configPath = Join-Path ${Env:UserProfile} ".ghidra"
+        New-Item -Path $configPath -ItemType directory -Force | Out-Null
+        $configPath = Join-Path $configPath ".$versionPath"
+        New-Item -Path $configPath -ItemType directory -Force | Out-Null
+        $configPath = Join-Path $configPath "java_home.save"
+        New-Item -Path $configPath -ItemType file -Force | Out-Null
+        Set-Content -Path $configPath -Value $openjdkPath -Force
+    } else {
+        $err_msg = "Could not find correct openjdk directory"
+        FE-Write-Log "ERROR" $err_msg
+    }
+  }
 } catch {
   VM-Write-Log-Exception $_
-}
-
-# Attempt to set JDK_HOME for Ghidra
-$openjdk_path = Join-Path ${Env:ProgramFiles} "OpenJDK"
-if (Test-Path $openjdk_path) {
-  $files = Get-ChildItem -Path $openjdk_path -Filter openjdk* | Sort-Object -Descending
-  if ($files.count -gt 0) {
-    $selected_dir = $files | Select-Object -first 1
-    $openjdk_path = Join-Path $openjdk_path $selected_dir
-    Install-ChocolateyEnvironmentVariable -VariableName "JDK_HOME" -VariableValue $openjdk_path -VariableType 'Machine'
-
-    # Add it do their config file as well
-    $config_path = Join-Path ${Env:UserProfile} ".ghidra"
-    New-Item -Path $config_path -ItemType directory -Force | Out-Null
-    $config_path = Join-Path $config_path ".$versionPath"
-    New-Item -Path $config_path -ItemType directory -Force | Out-Null
-    $config_path = Join-Path $config_path "java_home.save"
-    New-Item -Path $config_path -ItemType file -Force | Out-Null
-    Set-Content -Path $config_path -Value $openjdk_path -Force
-  } else {
-    $err_msg = "Could not find correct openjdk directory"
-    FE-Write-Log "ERROR" $err_msg
-  }
 }
