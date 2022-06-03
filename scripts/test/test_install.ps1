239d9996c18cd777b1b4966a1d6fbd8f1f083126
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2022-06-03 16:13:24 +0200

    Unify testing
    Avoid code duplication.
    
    Closes https://github.com/mandiant/VM-Packages/issues/18

--- scripts/test/test_install.ps1
+++ scripts/test/test_install.ps1
@@ -1,10 +1,11 @@
-## Run locally to test the packages, similar to .github/workflows/ci.yml
+# Build the packages in the 'packages' directory given as argument (or all if none provided) into the 'built_pkgs'.
+# Install the built packages
 
-# Save original location
-$org_dir = Get-Location
+# Examples
+## ./test_install
+## ./test_install '7zip.vm 010editor.vm'
 
-# Packages directory (e.g. the packages folder in the current directory)
-$packages_dir = 'packages'
+param ([string] $package_names=$null)
 
 # Error Code Definitions
 # ----------------------
@@ -15,63 +16,38 @@ $packages_dir = 'packages'
 # 3010: success, reboot required
 # other (not listed): likely an error has occurred
 $validExitCodes = @(0, 1605, 1614, 1641, 3010)
+$packages_dir_name = 'packages'
+$built_pkgs_dir_name = 'built_pkgs'
 
-# Ensure packages directory exists
-if (-Not (Test-Path $packages_dir)) { Exit 1 }
 
-# Create built packages directory and enumerate package directories
-$built_pkgs_dir = New-Item -Force -ItemType Directory 'built_pkgs'
-$package_dirs = Get-ChildItem -Path $packages_dir | ForEach-Object { $_.FullName }
+$root = Get-Location
+$built_pkgs_dir = New-Item -ItemType Directory -Force $built_pkgs_dir_name
 
-# Ask if packages should be built
-Write-Host -ForegroundColor Yellow -NoNewline "[INFO]: Build all packages? (y/n)"
-$response = Read-Host
-if ($response -eq 'y') {
-  # Build each package
-  foreach ($package_dir in $package_dirs) {
-      Set-Location $package_dir
-      choco pack -out $built_pkgs_dir
-      if ($LASTEXITCODE -ne 0) {
-        # Abort with the first failing build
-        Set-Location -Path $org_dir -PassThru | Out-Null
-        Exit 1
-      }
-  }
+if ($package_names) {
+    $packages = $package_names.Split(" ")
+} else {
+    $packages = Get-ChildItem -Path $packages_dir_name
 }
 
-# Change to built package directory
+foreach ($package in $packages) {
+    Set-Location "$root\$packages_dir_name\$package"
+    choco pack -out $built_pkgs_dir
+    if ($LASTEXITCODE -ne 0) { Exit 1 } # Abort with the first failing build
+}
+
+
+$built_pkgs = Get-ChildItem $built_pkgs_dir | Foreach-Object {( [regex]::match($_.BaseName, '(.*?[.](?:vm)).*').Groups[1].Value)}
 Set-Location $built_pkgs_dir
-
-# Ask if "common.vm" should explicitly be installed first
-Write-Host -ForegroundColor Yellow -NoNewline "[INFO]: Install common.vm package? (y/n)"
-$response = Read-Host
-if ($response -eq 'y') {
-  choco upgrade "common.vm" -force -y -s "'.;https://www.myget.org/F/vm-packages/api/v2'"
-}
-
-# Ask if we should continue install even if a package fails to install
-Write-Host -ForegroundColor Yellow -NoNewline "[INFO]: If package fails to install, continue installing other packages? (y/n)"
-$keep_installing_response = Read-Host
-
-# Install each package
-$built_pkgs = Get-ChildItem $built_pkgs -Recurse -Include *.nupkg | Foreach-Object {( [regex]::match($_.BaseName, '(.*?[.](?:vm)).*').Groups[1].Value)}
 foreach ($package in $built_pkgs) {
-  if ($package -LIKE "common.vm") {
-    continue
-  }
-  choco upgrade $package -force -y -s "'.;https://www.myget.org/F/vm-packages/api/v2;https://community.chocolatey.org/api/v2/'"
-  if ($validExitCodes -notcontains $LASTEXITCODE) {
-    if ($keep_installing_response -ne 'y') {
-      Write-Host -ForegroundColor Red -NoNewline "[WARN]: Install failed. Continue installing other packages? (y/n)"
-      $response = Read-Host
-      if ($response -ne 'y') {
+    choco install $package -y -s "'.;https://www.myget.org/F/vm-packages/api/v2;https://community.chocolatey.org/api/v2/'" --no-progress --force
+    if ($validExitCodes -notcontains $LASTEXITCODE) { # Abort with the first failing install
+        Write-Host -ForegroundColor Red "[ERROR] Failed to install $package"
+        # Restore the original location
         Set-Location -Path $org_dir -PassThru | Out-Null
-        Write-Host -ForegroundColor Red "[*] Exiting..."
         Exit 1
-      }
     }
-  }
 }
 
 # Restore the original location
-Set-Location -Path $org_dir -PassThru | Out-Null
+Set-Location -Path $root -PassThru | Out-Null
+
