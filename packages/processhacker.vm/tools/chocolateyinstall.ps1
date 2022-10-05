75b39f000aa3dc28b41883b2db721afe83de48ec
Dustin Silveri <dustin.silveri@gmail.com>
2022-10-05 09:42:20 -0700

    fix for build failure for processhacker2

--- packages/processhacker.vm/tools/chocolateyinstall.ps1
+++ packages/processhacker.vm/tools/chocolateyinstall.ps1
@@ -8,18 +8,18 @@ try {
   $url   = 'https://versaweb.dl.sourceforge.net/project/processhacker/processhacker2/processhacker-2.39-setup.exe'
   $checksum = '28042DD4A92A0033B8F1D419B9E989C5B8E32D1D2D881F5C8251D58CE35B9063'
 
+  $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+
   $packageArgs = @{
     packageName   = ${Env:ChocolateyPackageName}
     fileType      = 'EXE'
     url           = $url
     checksumType  = 'sha256'
     checksum      = $checksum
-    silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /NOICONS'
+    silentArgs    = "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /DIR=`"$toolDir`""
   }
   Install-ChocolateyPackage @packageArgs
-
-  $toolDir = Join-Path ${Env:ProgramFiles} $toolName -Resolve
-  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
 
   $executablePath = Join-Path $toolDir "$toolName.exe" -Resolve
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
