b30081699ddb97415594bd83663f80fc082d0639
vm-packages <vm-packages@mandiant.com>
2022-11-13 22:31:08 +0000

    Update cyberchef.vm to v9.49.0

--- packages/cyberchef.vm/tools/chocolateyinstall.ps1
+++ packages/cyberchef.vm/tools/chocolateyinstall.ps1
@@ -10,14 +10,14 @@ try {
   $packageArgs = @{
     packageName   = ${Env:ChocolateyPackageName}
     unzipLocation = $toolDir
-    url           = 'https://github.com/gchq/CyberChef/releases/download/v9.48.0/CyberChef_v9.48.0.zip'
-    checksum      = 'ce99537f5850904e6cd431bf48ef71896c9de69a12be1572f2f0582ad1e06504'
+    url           = 'https://github.com/gchq/CyberChef/releases/download/v9.49.0/CyberChef_v9.49.0.zip'
+    checksum      = '410f98fb8fc0e6d8eaf4bfa5c85422bf990f9e42df7e0859160d08fceb2c6570'
     checksumType  = 'sha256'
   }
   Install-ChocolateyZipPackage @packageArgs
   VM-Assert-Path $toolDir
 
-  $htmlPath = Join-Path $toolDir "CyberChef_v9.48.0.html" -Resolve
+  $htmlPath = Join-Path $toolDir "CyberChef_v9.49.0.html" -Resolve
   $shortcut = Join-Path $shortcutDir "CyberChef.lnk"
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $htmlPath
   VM-Assert-Path $shortcut
