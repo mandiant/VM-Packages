a458c5502711f2c808f5845f3864135c5556b2cf
MalwareMechanic <malwaremechanic@gmail.com>
2022-11-20 18:15:18 -0500

    Update category from Utils to Utilities

--- packages/vcbuildtools.vm/tools/chocolateyinstall.ps1
+++ packages/vcbuildtools.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} 'Utils'
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} 'Utilities'
 
   $programFiles = ${Env:ProgramFiles(x86)}
   if (-Not ($programFiles)) {
