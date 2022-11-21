4b2a1e0569c07dfa38fd3984891e18acd0aaaa59
MalwareMechanic <malwaremechanic@gmail.com>
2022-11-21 09:33:38 -0500

    Update category from Utils to Utilities (#106)
    * Update category from Utils to Utilities
    
    * Update nuspec version

--- packages/vcbuildtools.vm/tools/chocolateyuninstall.ps1
+++ packages/vcbuildtools.vm/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
 ﻿$ErrorActionPreference = 'Continue'
-$shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} 'Utils'
+$shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} 'Utilities'
 $shortcut = Join-Path $shortcutDir 'Microsoft Visual C++ Build Tools.lnk'
 Remove-Item $shortcut -Force -ea 0 | Out-Null
