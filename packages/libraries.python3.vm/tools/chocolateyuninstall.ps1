68342319f6f8114dea0bd1049ad03546c4b6d31a
MalwareMechanic <malwaremechanic@gmail.com>
2022-11-28 21:14:30 -0500

    Update libraries.python* packages to use XML

--- packages/libraries.python3.vm/tools/chocolateyuninstall.ps1
+++ packages/libraries.python3.vm/tools/chocolateyuninstall.ps1
@@ -1,54 +1,39 @@
 $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
-function LoadPackages {
-  try {
-    $json = Get-Content $pkgPath -ErrorAction Stop
-    $packages = VM-ConvertFrom-Json $json
-  } catch {
-    return $null
-  }
-  return $packages
+try {
+    $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $modulesPath = Join-Path $toolDir "modules.xml" -Resolve
+    $modulesXml = [xml](Get-Content $modulesPath)
+
+    # Create output file to log python module uninstallation details
+    $outputFile = VM-New-Install-Log $toolDir
+
+    # Upgrade pip
+    Invoke-Expression "py -3 -m pip install -qq --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
+
+    $success = $true
+    $modules = $modulesXml.modules.module
+    foreach ($module in $modules) {
+        VM-Write-Log "INFO" "Attempting to uninstall Python3 module: $($module.name)"
+
+        Invoke-Expression "py -3 -m pip uninstall -y $($module.name) 2>&1 >> $outputFile"
+
+        if ($LastExitCode -eq 0) {
+            VM-Write-Log "INFO" "Uninstalled Python3 module: $($module.name)"
+        } else {
+            VM-Write-Log "ERROR" "Failed to uninstall Python3 module: $($module.name)"
+            $success = $false
+        }
+    }
+
+    if ($success -eq $false) {
+        VM-Write-Log "ERROR" "Failed to uninstall at least one Python3 module"
+        $outputFile = $outputFile.replace('lib\', 'lib-bad\')
+        VM-Write-Log "ERROR" "Check $outputFile for more information"
+        VM-Write-Log "ERROR" "Please manually uninstall the remaining Python3 modules via:`n`tpy -3 -m pip uninstall -y <module_name>"
+    }
+} catch {
+    VM-Write-Log-Exception $_
 }
 
-function UninstallOneModule {
-  param([string] $name)
-  Invoke-Expression "py -3 -m pip uninstall -y $name 2>&1 >> $outputFile"
-  return $LastExitCode
-}
-
-$toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$pkgPath = Join-Path $toolDir "packages.json"
-
-$json = LoadPackages $pkgPath
-if (($null -eq $json) -Or ($null -eq $json.packages)) {
-  Write-Host "Error loading packages! Exiting"
-  Exit 1
-}
-
-# Create output file to log python module installation details
-$outputFile = VM-New-Install-Log $toolDir
-
-# upgrade pip
-Invoke-Expression "py -3 -m pip install --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
-
-$packages = $json.packages
-$success = $true
-foreach ($pkg in $packages) {
-  $name = $pkg.name
-  Write-Host "Trying to uninstall $name"
-  if ((UninstallOneModule $name) -eq 0) {
-    Write-Host "Uninstalled $name"
-  } else {
-    VM-Write-Log "ERROR" "Failed to uninstall $name"
-    $success = $false
-  }
-}
-
-if ($success -eq $false) {
-  Write-Host "Failed to uninstall at least one Python module"
-  VM-Write-Log "ERROR" "Check $outputFile for more information"
-  Exit 1
-} else {
-  Remove-Item -Path $outputFile -Force | Out-Null
-}
