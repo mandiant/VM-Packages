76cc66deb6e39616226a4528e41d01af05589f89
MalwareMechanic <malwaremechanic@gmail.com>
2022-11-29 12:59:16 -0500

    Update libraries.python* packages to use XML (#120)

--- packages/libraries.python3.vm/tools/chocolateyinstall.ps1
+++ packages/libraries.python3.vm/tools/chocolateyinstall.ps1
@@ -1,63 +1,43 @@
 $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
-function LoadPackages {
-  try {
-    $json = Get-Content $pkgPath -ErrorAction Stop
-    $packages = VM-ConvertFrom-Json $json
-  } catch {
-    return $null
-  }
-  return $packages
-}
-
-function InstallOneModule {
-  param([hashtable] $module)
-  $name = $module.name
-  if ($null -eq $module.url) {
-    $url = $name
-  } else {
-    $url = $module.url
-  }
-  Invoke-Expression "py -3.9 -m pip install $url 2>&1 >> $outputFile"
-  return $LastExitCode
-}
-
 try {
-  $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $pkgPath = Join-Path $toolDir "packages.json"
+    $toolDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $modulesPath = Join-Path $toolDir "modules.xml" -Resolve
+    $modulesXml = [xml](Get-Content $modulesPath)
 
-  $json = LoadPackages $pkgPath
-  if (($null -eq $json) -Or ($null -eq $json.packages)) {
-    VM-Write-Log "ERROR" "Error loading packages! Exiting"
-    Exit 1
-  }
+    # Create output file to log python module installation details
+    $outputFile = VM-New-Install-Log $toolDir
 
-  # Create output file to log python module installation details
-  $outputFile = VM-New-Install-Log $toolDir
+    # Upgrade pip
+    Invoke-Expression "py -3 -m pip install -qq --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
 
-  # upgrade pip
-  Invoke-Expression "py -3.9 -m pip install --no-cache-dir --upgrade pip 2>&1 >> $outputFile"
+    $success = $true
+    $modules = $modulesXml.modules.module
+    foreach ($module in $modules) {
+        VM-Write-Log "INFO" "Attempting to install Python3 module: $($module.name)"
+        $intallValue = $module.name
+        if ($module.url) {
+            $intallValue = $module.url
+        }
 
-  $packages = $json.packages
-  $success = $true
-  foreach ($pkg in $packages) {
-    $name = $pkg.name
-    Write-Host "Trying to install $name"
-    if ((InstallOneModule $pkg) -eq 0) {
-      Write-Host "Installed $name"
-    } else {
-      VM-Write-Log "ERROR" "Failed to install $name"
-      $success = $false
+        Invoke-Expression "py -3 -m pip install $intallValue 2>&1 >> $outputFile"
+
+        if ($LastExitCode -eq 0) {
+            VM-Write-Log "INFO" "Installed Python3 module: $($module.name)"
+        } else {
+            VM-Write-Log "ERROR" "Failed to install Python3 module: $($module.name)"
+            $success = $false
+        }
     }
-  }
 
-  if ($success -eq $false) {
-    VM-Write-Log "ERROR" "Failed to install at least one Python module"
-    $outputFile = $outputFile.replace('lib\', 'lib-bad\')
-    VM-Write-Log "ERROR" "Check $outputFile for more information"
-    Exit 1
-  }
+    if ($success -eq $false) {
+        VM-Write-Log "ERROR" "Failed to install at least one Python3 module"
+        $outputFile = $outputFile.replace('lib\', 'lib-bad\')
+        VM-Write-Log "ERROR" "Check $outputFile for more information"
+        exit 1
+    }
 } catch {
-  VM-Write-Log-Exception $_
+    VM-Write-Log-Exception $_
 }
+
