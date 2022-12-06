1796acd0b3b2cbb495fec3ef932c1bd5fc802239
Moritz <mr-tz@users.noreply.github.com>
2022-12-06 13:54:02 +0100

    downloaded ZIP contains inner folder with binaries

--- packages/networkminer.vm/tools/chocolateyinstall.ps1
+++ packages/networkminer.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $category = 'Networking'
 $zipUrl = 'https://www.netresec.com/?download=NetworkMiner'
 $zipSha256 = 'cf477b651c3bcc70d6f5d50f9bdcb6d8cf2dd85b7018109ff474b9df3c7a0f7e'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
