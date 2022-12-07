af74314a0f2841962b4962dbf624a96b15e3f4ba
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-07 08:28:53 -0600

    Rename to lowercase, add innerzip flag

--- packages/cutter.vm/tools/chocolateyinstall.ps1
+++ packages/cutter.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $category = 'Disassemblers'
 $zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.1.2/Cutter-v2.1.2-Windows-x86_64.zip'
 $zipSha256 = '9BD08FC9D5591149D3E6FEE3E269458E3781706C09C2C1FF49355E48D27C0486'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
