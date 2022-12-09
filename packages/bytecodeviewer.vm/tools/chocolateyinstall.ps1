ee2a7d6ffd97d81b56f5c59299131ebc28755b72
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-09 10:27:28 -0600

    Add javaruntime dependency and update install function

--- packages/bytecodeviewer.vm/tools/chocolateyinstall.ps1
+++ packages/bytecodeviewer.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Bytecode Viewer'
 $category = 'Java'
 
-$zipUrl = 'https://github.com/Konloch/bytecode-viewer/releases/download/v2.11.2/Bytecode-Viewer-2.11.2.jar'
-$zipSha256 = '536ad387424106083f76cd0cb7c051a22aff21f08663ba2539c11f1ddef9147f'
+$exeUrl = 'https://github.com/Konloch/bytecode-viewer/releases/download/v2.11.2/Bytecode-Viewer-2.11.2.jar'
+$exeSha256 = '536ad387424106083f76cd0cb7c051a22aff21f08663ba2539c11f1ddef9147f'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256
