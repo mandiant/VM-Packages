3161b758b3a98be9142bc2e05c5a6bc21a7d84b8
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-09 16:06:50 -0600

    Add fiddler classic

---
+++ packages/fiddlerclassic.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+    $toolName = 'fiddler'
+    $category = 'Networking'
+
+    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+    $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+    $executablePath = Join-Path ${Env:LOCALAPPDATA} "Programs\Fiddler\fiddler.exe" -Resolve
+    Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
+    VM-Assert-Path $shortcut
+
+    Install-BinFile -Name $toolName -Path $executablePath
+} catch {
+    VM-Write-Log-Exception $_
+}
+
