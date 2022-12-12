988511841b2d8dfdeedaca42148965a81ff46fbf
Moritz <mr-tz@users.noreply.github.com>
2022-12-12 18:14:36 +0100

    Update chocolateyinstall.ps1

--- packages/pebear.vm/tools/chocolateyinstall.ps1
+++ packages/pebear.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'PE-bear'
   $category = 'PE'
-  $shimPath = '%PROGRAMDATA%\chocolatey\bin\PE-bear.exe'
+  $shimPath = 'bin\PE-bear.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
