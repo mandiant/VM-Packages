eb2665d22a72f93b8bb637249104bffdd9bbf893
vm-packages <vm-packages@mandiant.com>
2022-12-13 08:41:23 +0000

    Add exiftool.vm
    Closes https://github.com/mandiant/VM-Packages/issues/182.

---
+++ packages/exiftool.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,16 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = 'exiftool'
+  $category = 'Utilities'
+  $shimPath = 'bin\exiftool.exe'
+
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
+  VM-Assert-Path $shortcut
+} catch {
+  VM-Write-Log-Exception $_
+}
---
+++ packages/exiftool.vm/tools/chocolateyuninstall.ps1
@@ -0,0 +1,7 @@
+$ErrorActionPreference = 'Continue'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'exiftool'
+$category = 'Utilities'
+
+VM-Remove-Tool-Shortcut $toolName $category
