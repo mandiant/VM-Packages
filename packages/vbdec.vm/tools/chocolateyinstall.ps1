106d6e74ca137f81a911eaab0bec9fd24b8796ea
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-20 10:59:47 -0500

    Add common native installer function
    New function assists with native installers (NSIS, Inno Setup, MSI, etc)
    Updated package to use new function

--- packages/vbdec.vm/tools/chocolateyinstall.ps1
+++ packages/vbdec.vm/tools/chocolateyinstall.ps1
@@ -1,10 +1,20 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
-$toolName = 'vbdec'
-$category = 'VB'
+try {
+    $toolName = 'vbdec'
+    $category = 'VB'
 
-$exeUrl = 'https://github.com/dzzie/pdfstreamdumper/releases/download/vbdec_12.7.22/VBDEC_Setup_SnapShot_12.8.22.exe'
-$exeSha256 = 'BAED0DA101D1C5D5A326D5C6D004C811C9D23CB76638F79EAFFA9150DB7E8535'
+    $exeUrl = 'https://github.com/dzzie/pdfstreamdumper/releases/download/vbdec_12.7.22/VBDEC_Setup_SnapShot_12.8.22.exe'
+    $exeSha256 = 'BAED0DA101D1C5D5A326D5C6D004C811C9D23CB76638F79EAFFA9150DB7E8535'
 
-VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256
+    $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
+    $executablePath = (Join-Path $toolDir "vbdec.exe")
+    VM-Install-With-Installer $toolName $category "EXE" "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Dir=`"$($toolDir)`"" $executablePath $exeUrl -sha256 $zipSha256
+
+    $desktopShortcut = Join-Path $([Environment]::GetFolderPath("Desktop")) "VBDEC.exe.lnk"
+    Remove-Item $desktopShortcut -Force -ea 0
+} catch {
+    VM-Write-Log-Exception $_
+}
+
--- packages/vbdec.vm/tools/chocolateyuninstall.ps1
+++ packages/vbdec.vm/tools/chocolateyuninstall.ps1
@@ -4,4 +4,9 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'vbdec'
 $category = 'VB'
 
+# Silently uninstall
+VM-Uninstall-With-Uninstaller $toolName "EXE" "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-"
+
+# Remove directory, shortcut, shim
 VM-Uninstall $toolName $category
+
