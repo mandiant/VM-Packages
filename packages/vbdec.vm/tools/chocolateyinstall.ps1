e56b90bc82976968bcbf66ddd8cf66705b8f203f
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-20 11:06:51 -0500

    Fix linting issues

--- packages/vbdec.vm/tools/chocolateyinstall.ps1
+++ packages/vbdec.vm/tools/chocolateyinstall.ps1
@@ -6,11 +6,11 @@ try {
     $category = 'VB'
 
     $exeUrl = 'https://github.com/dzzie/pdfstreamdumper/releases/download/vbdec_12.7.22/VBDEC_Setup_SnapShot_12.8.22.exe'
-    $exeSha256 = 'BAED0DA101D1C5D5A326D5C6D004C811C9D23CB76638F79EAFFA9150DB7E8535'
+    $exeSha256 = 'baed0da101d1c5d5a326d5c6d004c811c9d23cb76638f79eaffa9150db7e8535'
 
     $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
     $executablePath = (Join-Path $toolDir "vbdec.exe")
-    VM-Install-With-Installer $toolName $category "EXE" "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Dir=`"$($toolDir)`"" $executablePath $exeUrl -sha256 $zipSha256
+    VM-Install-With-Installer $toolName $category "EXE" "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP- /Dir=`"$($toolDir)`"" $executablePath $exeUrl -sha256 $exeSha256
 
     $desktopShortcut = Join-Path $([Environment]::GetFolderPath("Desktop")) "VBDEC.exe.lnk"
     Remove-Item $desktopShortcut -Force -ea 0
