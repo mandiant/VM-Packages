0b321042c6e66690c228c46889d05d023cc68a10
MalwareMechanic <malwaremechanic@gmail.com>
2022-12-23 12:47:34 -0500

    Add nmap package (#197)
    Closes #195

---
+++ packages/nmap.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,54 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+    $toolName = 'nmap'
+    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} 'Networking'
+    $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
+
+    $exeUrl = 'https://nmap.org/dist/nmap-7.93-setup.exe'
+    $exeSha256 = 'f1160a33fb79c764cdc4c023fa700054ae2945ed91880e37348a17c010ca716f'
+    $installerName = Split-Path -Path $exeUrl -Leaf
+
+    $packageArgs = @{
+        packageName   = ${Env:ChocolateyPackageName}
+        url           = $exeUrl
+        checksum      = $exeSha256
+        checksumType  = "sha256"
+        fileFullPath  = Join-Path ${Env:TEMP} $installerName
+    }
+    Get-ChocolateyWebFile @packageArgs
+    VM-Assert-Path $packageArgs.fileFullPath
+
+    # AutoHotkey the installer
+    $ahkInstaller = Join-Path $(Split-Path $MyInvocation.MyCommand.Definition) "install.ahk" -Resolve
+    $rc = (Start-Process -FilePath $ahkInstaller -ArgumentList "$($packageArgs.fileFullPath) $toolDir" -PassThru -Wait).ExitCode
+    if ($rc -eq 1) {
+        throw "AutoHotKey returned a failure exit code ($rc) for: ${Env:ChocolateyPackageName}"
+    } else {
+        VM-Assert-Path $(Join-Path $toolDir "nmap.exe")
+    }
+
+    # Add shortcuts for all the EXE's (besides Zenmap since it's not a console application)
+    $exePaths = Get-ChildItem $toolDir | Where-Object { $_.Name -match '^.*(?<!Uninstall|zenmap)\.exe$' }
+    $executableCmd  = Join-Path ${Env:WinDir} "system32\cmd.exe"
+    $executableDir  = Join-Path ${Env:UserProfile} "Desktop"
+    foreach ($exe in $exePaths) {
+        $shortcut = Join-Path $shortcutDir "$($exe.Basename).lnk"
+        $executablePath = $exe.FullName
+        $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`" --help`""
+        Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir -RunAsAdmin
+        VM-Assert-Path $shortcut
+        Install-BinFile -Name $exe.Basename -Path $exe.FullName
+    }
+
+    # Add shortcut for Zenmap
+    $executablePath = Join-Path $toolDir "zenmap.exe" -Resolve
+    $shortcut = Join-Path $shortcutDir "zenmap.lnk"
+    Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -WorkingDirectory $executableDir -RunAsAdmin
+    VM-Assert-Path $shortcut
+    Install-BinFile -Name "zenmap" -Path $executablePath
+} catch {
+    VM-Write-Log-Exception $_
+}
+
---
+++ packages/nmap.vm/tools/install.ahk
@@ -0,0 +1,64 @@
+#NoEnv                       ; Recommended for performance and compatibility with future AutoHotkey releases.
+#Warn                        ; Enable warnings to assist with detecting common errors.
+#SingleInstance, force       ; Ensure a single instance of this script
+SendMode Input               ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+SetTitleMatchMode, RegEx     ; Enable regular expressions in title matches
+
+; Handle installation
+installer = %1%
+Run, %installer%
+installerTitle := "i)nmap setup"
+WinWait, %installerTitle%,,20
+WinActivate
+
+exitCode := 1
+loop, 20
+{
+    if WinExist(installerTitle, "i).*license agreement.*")
+    {
+        BlockInput, On
+        WinActivate
+        Sleep, 500
+        ControlSend,, {Enter}
+        Sleep, 500
+        ControlSend,, {Enter}
+        Sleep, 500
+        SendInput, %2%
+        Sleep, 500
+        ControlSend,, {Enter}
+        BlockInput, Off
+    }
+    if WinExist(installerTitle, "i).*installing.*")
+    {
+        Sleep, 5000
+    }
+    if WinExist(installerTitle, "i).*installation complete.*")
+    {
+        BlockInput, On
+        WinActivate
+        Sleep, 500
+        ControlSend,, {Enter}
+        Sleep, 500
+        Send, {Tab}
+        Sleep, 100
+        Send, {Space}
+        Sleep, 100
+        Send, {Tab}
+        Sleep, 100
+        Send, {Tab}
+        Sleep, 100
+        Send, {Tab}
+        Sleep, 100
+        Send, {Space}
+        Sleep, 100
+        ControlSend,, {Enter}
+        Sleep, 500
+        ControlSend,, {Enter}
+        BlockInput, Off
+        exitCode := 0
+        break
+    }
+    Sleep 1000
+}
+ExitApp %exitCode%
