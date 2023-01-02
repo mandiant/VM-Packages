0bd3f146cd7122269512801096e31954bfe598e5
Moritz <mr-tz@users.noreply.github.com>
2023-01-02 15:35:37 +0100

    Update chocolateyinstall.ps1

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $category = 'PE'
 $zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
 $zipSha256 = '0B5605B47A75C10F0988928979E1B8730EF85E876C4F6276A09D04AB1A96833E'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
