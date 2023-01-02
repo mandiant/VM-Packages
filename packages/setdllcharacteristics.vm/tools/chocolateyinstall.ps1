7aec3da1daa07ca6a8bbd229528dfa63326cfbf0
vm-packages <vm-packages@mandiant.com>
2023-01-02 15:17:06 +0000

    Add setdllcharacteristics.vm
    Closes https://github.com/mandiant/VM-Packages/issues/185.

---
+++ packages/setdllcharacteristics.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'setdllcharacteristics'
+$category = 'PE'
+
+$zipUrl = 'http://www.didierstevens.com/files/software/setdllcharacteristics_v0_0_0_1.zip'
+$zipSha256 = '5a9d3815f317c7c0ff7737f271ce0c60be2cb0f4168c5ea5ad8cef84ad718577'
+
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
