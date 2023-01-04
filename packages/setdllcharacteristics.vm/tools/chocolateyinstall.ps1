f7ce6aef9c158ffbb915c3989a5ed1dfaa467265
MalwareMechanic <malwaremechanic@gmail.com>
2023-01-04 17:43:21 -0500

    Change to HTTPS and enable console app mode

--- packages/setdllcharacteristics.vm/tools/chocolateyinstall.ps1
+++ packages/setdllcharacteristics.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,8 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'setdllcharacteristics'
 $category = 'PE'
 
-$zipUrl = 'http://www.didierstevens.com/files/software/setdllcharacteristics_v0_0_0_1.zip'
+$zipUrl = 'https://www.didierstevens.com/files/software/setdllcharacteristics_v0_0_0_1.zip'
 $zipSha256 = '5a9d3815f317c7c0ff7737f271ce0c60be2cb0f4168c5ea5ad8cef84ad718577'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+
