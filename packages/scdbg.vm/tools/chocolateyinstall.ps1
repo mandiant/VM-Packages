1769e673c65cd106dc6bc2179fba0a5e7269616e
MalwareMechanic <malwaremechanic@gmail.com>
2023-01-04 17:35:10 -0500

    Add GUI variant of scdbg as well

--- packages/scdbg.vm/tools/chocolateyinstall.ps1
+++ packages/scdbg.vm/tools/chocolateyinstall.ps1
@@ -1,10 +1,23 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
-$toolName = 'scdbg'
-$category = 'Debuggers'
+try {
+    $toolName = 'scdbg'
+    $category = 'Debuggers'
 
-$zipUrl = 'https://github.com/dzzie/VS_LIBEMU/releases/download/12.7.22/VS_LIBEMU_12_7_22.zip'
-$zipSha256 = '521130E34CC0A30587FF99D030633B9D124CCAC779A213E15025535171B4113D'
+    $zipUrl = 'https://github.com/dzzie/VS_LIBEMU/releases/download/12.7.22/VS_LIBEMU_12_7_22.zip'
+    $zipSha256 = '521130E34CC0A30587FF99D030633B9D124CCAC779A213E15025535171B4113D'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+    VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+
+    # Add GUI variant of scdbg
+    $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
+    $executablePath = Join-Path $toolDir 'gui_launcher.exe' -Resolve
+    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+    $shortcut = Join-Path $shortcutDir 'scdbg_gui.lnk'
+    Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath
+    Install-BinFile -Name 'scdbg_gui' -Path $executablePath
+} catch {
+    VM-Write-Log-Exception $_
+}
+
--- packages/scdbg.vm/tools/chocolateyuninstall.ps1
+++ packages/scdbg.vm/tools/chocolateyuninstall.ps1
@@ -5,3 +5,8 @@ $toolName = 'scdbg'
 $category = 'Debuggers'
 
 VM-Uninstall $toolName $category
+
+# Remove GUI variant of scdbg
+VM-Remove-Tool-Shortcut 'scdbg_gui' $category
+Uninstall-BinFile -Name 'scdbg_gui'
+
