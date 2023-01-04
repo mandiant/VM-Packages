7a4589f6a8ef592c0c8cda03377904b929b149ad
MalwareMechanic <malwaremechanic@gmail.com>
2023-01-04 17:50:50 -0500

    Enable console app mode

--- packages/hollowshunter.vm/tools/chocolateyinstall.ps1
+++ packages/hollowshunter.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,5 @@ $category = 'PE'
 $zipUrl = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.3.5/hollows_hunter32.zip'
 $zipSha256 = '0bbb0b794ac0b1c179a993a7bd26489fc1b8497aca05fa797b49f3d9bc71a6ca'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+
