db01dc9a6aa8ec31d0649b2567fde19fe9d904dd
mr-tz <moritz.raabe@mandiant.com>
2023-01-26 16:56:06 +0100

    enable debug

--- scripts/test/lint.py
+++ scripts/test/lint.py
@@ -26,7 +26,7 @@ from xml.dom import minidom
 GIT_EXE = "git"
 
 # set log level for debugging here script-wide
-LOG_LEVEL = logging.INFO
+LOG_LEVEL = logging.DEBUG
 logging.basicConfig(level=LOG_LEVEL)
 logger = logging.getLogger("lint")
 
