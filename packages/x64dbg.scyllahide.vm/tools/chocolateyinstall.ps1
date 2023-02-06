53280338486fdba3a7f49a61b8ea3a27c11cb897
MalwareMechanic <malwaremechanic@gmail.com>
2023-02-06 11:53:09 -0500

    Add minor updates to handle x64dbg plugin

--- packages/x64dbg.scyllahide.vm/tools/chocolateyinstall.ps1
+++ packages/x64dbg.scyllahide.vm/tools/chocolateyinstall.ps1
@@ -2,54 +2,34 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  VM-Remove-PreviousZipPackage ${Env:chocolateyPackageFolder}
+    $toolSrcDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $toolSrcDir = Join-Path $toolSrcDir 'ScyllaHide'
 
-  $toolSrcDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $toolSrcDir = Join-Path $toolSrcDir 'ScyllaHide'
+    $packageArgs = @{
+        packageName   = ${Env:ChocolateyPackageName}
+        unzipLocation = $toolSrcDir
+        url           = 'https://github.com/x64dbg/ScyllaHide/releases/download/snapshot-2021-08-23_13-27-50/ScyllaHide.7z'
+        checksum      = 'c51929341ff726d219e670928433a176e114ca9a4c36f416629aef50c98b8817'
+        checksumType  = 'sha256'
+    }
+    Install-ChocolateyZipPackage @packageArgs
+    VM-Assert-Path $toolSrcDir
 
-  $packageArgs = @{
-    packageName   = ${Env:ChocolateyPackageName}
-    unzipLocation = $toolSrcDir
-    url           = 'https://github.com/x64dbg/ScyllaHide/releases/download/snapshot-2021-08-23_13-27-50/ScyllaHide.7z'
-    checksum      = 'c51929341ff726d219e670928433a176e114ca9a4c36f416629aef50c98b8817'
-    checksumType  = 'sha256'
-  }
+    $archs = @("x32", "x64")
+    foreach ($arch in $archs) {
+        $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} "x64dbg\release\$arch" -Resolve
+        $toolDstDir = Join-Path $toolDstDir 'plugins'
+        if (-Not (Test-Path $toolDstDir -PathType Container)) {
+            New-Item -ItemType directory $toolDstDir -Force -ea 0 | Out-Null
+        }
+        VM-Assert-Path $toolDstDir
 
-  # Create $toolSrcDir if not exist
-  if (-Not (Test-Path $toolSrcDir)) {
-      New-Item -ItemType directory $toolSrcDir -Force -ea 0
-  }
-  VM-Assert-Path $toolSrcDir
+        # Move plugin into the tool directory
+        $pluginSrcPath = Join-Path $toolSrcDir "x64dbg\$arch\plugins" -Resolve
+        Get-ChildItem -Path $pluginSrcPath -Recurse -File | Move-Item -Destination $toolDstDir -Force -ea 0
+    }
 
-  Install-ChocolateyZipPackage @packageArgs
-
-  $pluginSrcPath = Join-Path $toolSrcDir 'x64dbg\x32\plugins' -Resolve
-  $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} 'x64dbg\release\x32' -Resolve
-
-  $toolDstDir = Join-Path $toolDstDir 'plugins' -Resolve
-  if ( -Not (Test-Path $toolDstDir -PathType Container)) {
-    New-Item -ItemType directory $toolDstDir -Force -ea 0
-  }
-  VM-Assert-Path $toolDstDir
-
-  # Move x32 plugin into the tool directory
-  $pluginDstPath = Join-Path $toolDstDir $pluginFile
-  Get-ChildItem -Path $pluginSrcPath -Recurse -File | Move-Item -Destination $pluginDstPath -Force -ea 0
-  VM-Assert-Path $pluginDstPath
-
-  $pluginSrcPath = Join-Path $toolSrcDir 'x64dbg\x64\plugins' -Resolve
-  $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} 'x64dbg\release\x64' -Resolve
-
-  $toolDstDir = Join-Path $toolDstDir 'plugins' -Resolve
-  if ( -Not (Test-Path $toolDstDir -PathType Container)) {
-    New-Item -ItemType directory $toolDstDir -Force -ea 0
-  }
-  VM-Assert-Path $toolDstDir
-
-  # Move x64 plugin into the tool directory
-  $pluginDstPath = Join-Path $toolDstDir $pluginFile
-  Get-ChildItem -Path $pluginSrcPath -Recurse -File | Move-Item -Destination $pluginDstPath -Force -ea 0
-  VM-Assert-Path $pluginDstPath
+    Remove-Item -Path $toolSrcDir -Recurse -Force -ea 0
 } catch {
-  VM-Write-Log-Exception $_
+    VM-Write-Log-Exception $_
 }
