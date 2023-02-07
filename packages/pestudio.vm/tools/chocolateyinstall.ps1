1abc5b952fac4a43ed2b08225b482520ee02bbb3
dkay0670 <63021402+dkay0670@users.noreply.github.com>
2023-02-07 03:27:41 -0800

    Update pestudio.vm to 9.47 (#232)
    * Update hash to match new PEStudio version
    
    PEStudio was updated and the hash no longer matches.

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'pestudio'
 $category = 'PE'
 
 $zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
-$zipSha256 = '0B5605B47A75C10F0988928979E1B8730EF85E876C4F6276A09D04AB1A96833E'
+$zipSha256 = 'E1CA8E305C080EF6880173BD7F17F7E4773C420F8545DB9A72F6958F1AEDDC0A'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
