37ed950b3963a4bc18bcd5c6602eaf641353beca
TheoKlein <theokleintw@gmail.com>
2023-02-09 16:55:23 +0800

    update installation script for ollydbg.scyllahide.vm

--- packages/ollydbg.scyllahide.vm/tools/chocolateyinstall.ps1
+++ packages/ollydbg.scyllahide.vm/tools/chocolateyinstall.ps1
@@ -2,39 +2,31 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  VM-Remove-PreviousZipPackage ${Env:chocolateyPackageFolder}
+    VM-Remove-PreviousZipPackage ${Env:chocolateyPackageFolder}
 
-  $toolSrcDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-  $toolSrcDir = Join-Path $toolSrcDir 'ScyllaHide'
+    $toolSrcDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+    $toolSrcDir = Join-Path $toolSrcDir 'ScyllaHide'
 
-  $packageArgs = @{
+    $packageArgs = @{
     packageName   = ${Env:ChocolateyPackageName}
-    unzipLocation = $toolSrcDir
-    url           = 'https://github.com/x64dbg/ScyllaHide/releases/download/snapshot-2021-08-23_13-27-50/ScyllaHide.7z'
-    checksum      = 'c51929341ff726d219e670928433a176e114ca9a4c36f416629aef50c98b8817'
-    checksumType  = 'sha256'
-  }
+        unzipLocation = $toolSrcDir
+        url           = 'https://github.com/x64dbg/ScyllaHide/releases/download/snapshot-2021-08-23_13-27-50/ScyllaHide.7z'
+        checksum      = 'c51929341ff726d219e670928433a176e114ca9a4c36f416629aef50c98b8817'
+        checksumType  = 'sha256'
+    }
 
-  # Create $toolSrcDir if not exist
-  if (-Not (Test-Path $toolSrcDir)) {
-      New-Item -ItemType directory $toolSrcDir -Force -ea 0
-  }
-  VM-Assert-Path $toolSrcDir
+    Install-ChocolateyZipPackage @packageArgs
+    VM-Assert-Path $toolSrcDir
 
-  Install-ChocolateyZipPackage @packageArgs
+    $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} 'OllyDbg' -Resolve
+    if (-Not (Test-Path $toolDstDir -PathType Container)) {
+        New-Item -ItemType directory $toolDstDir -Force -ea 0
+    }
+    VM-Assert-Path $toolDstDir
 
-  $pluginSrcPath = Join-Path $toolSrcDir 'Olly1' -Resolve
-  $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} 'OllyDbg' -Resolve
-
-  if ( -Not (Test-Path $toolDstDir -PathType Container)) {
-    New-Item -ItemType directory $toolDstDir -Force -ea 0
-  }
-  VM-Assert-Path $toolDstDir
-
-  # Move plugin into the tool directory
-  $pluginDstPath = Join-Path $toolDstDir $pluginFile
-  Get-ChildItem -Path $pluginSrcPath -Recurse -File | Move-Item -Destination $pluginDstPath -Force -ea 0
-  VM-Assert-Path $pluginDstPath
+    # Move plugin into the tool directory
+    $pluginSrcPath = Join-Path $toolSrcDir "Olly1" -Resolve
+    Get-ChildItem -Path $pluginSrcPath -Recurse -File | Move-Item -Destination $toolDstDir -Force -ea 0
 } catch {
-  VM-Write-Log-Exception $_
+    VM-Write-Log-Exception $_
 }
