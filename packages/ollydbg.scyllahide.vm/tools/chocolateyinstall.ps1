a1876d31fc2bb55177354d5e53cd380dbcb62b3a
TheoKlein <theokleintw@gmail.com>
2023-02-10 09:48:39 +0800

    update installation script for ollydbg.scyllahide.vm

--- packages/ollydbg.scyllahide.vm/tools/chocolateyinstall.ps1
+++ packages/ollydbg.scyllahide.vm/tools/chocolateyinstall.ps1
@@ -2,13 +2,11 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-    VM-Remove-PreviousZipPackage ${Env:chocolateyPackageFolder}
-
     $toolSrcDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
     $toolSrcDir = Join-Path $toolSrcDir 'ScyllaHide'
 
     $packageArgs = @{
-    packageName   = ${Env:ChocolateyPackageName}
+        packageName   = ${Env:ChocolateyPackageName}
         unzipLocation = $toolSrcDir
         url           = 'https://github.com/x64dbg/ScyllaHide/releases/download/snapshot-2021-08-23_13-27-50/ScyllaHide.7z'
         checksum      = 'c51929341ff726d219e670928433a176e114ca9a4c36f416629aef50c98b8817'
@@ -20,7 +18,7 @@ try {
 
     $toolDstDir = Join-Path ${Env:RAW_TOOLS_DIR} 'OllyDbg' -Resolve
     if (-Not (Test-Path $toolDstDir -PathType Container)) {
-        New-Item -ItemType directory $toolDstDir -Force -ea 0
+        New-Item -ItemType directory $toolDstDir -Force -ea 0 | Out-Null
     }
     VM-Assert-Path $toolDstDir
 
