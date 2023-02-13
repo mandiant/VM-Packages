bf96506e5c170343083e3654e9d18825bbb32df1
mr-tz <moritz.raabe@mandiant.com>
2023-02-13 15:17:26 +0100

    update version and make package more flexible

--- packages/ghidra.vm/tools/chocolateyinstall.ps1
+++ packages/ghidra.vm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ try {
     $toolName = 'ghidra'
     $category = 'Disassemblers'
     $shimPath = 'bin\ghidra.exe'
-    $versionPath = 'ghidra_10.2.2_PUBLIC'
+    $versionPath = 'ghidra_' + $env:ChocolateyPackageVersion + '_PUBLIC'
 
     $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
     # Get path to ghidra dependency directory
