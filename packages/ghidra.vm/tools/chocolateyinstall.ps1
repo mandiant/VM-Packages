3434eb0314cfda0e3a452663143ce655e025d108
MalwareMechanic <malwaremechanic@gmail.com>
2023-02-14 16:27:38 -0500

    Update packages/ghidra.vm/tools/chocolateyinstall.ps1
    Co-authored-by: Moritz <mr-tz@users.noreply.github.com>

--- packages/ghidra.vm/tools/chocolateyinstall.ps1
+++ packages/ghidra.vm/tools/chocolateyinstall.ps1
@@ -5,7 +5,7 @@ try {
     $toolName = 'ghidra'
     $category = 'Disassemblers'
     $shimPath = 'bin\ghidra.exe'
-    $versionPath = 'ghidra_' + $env:ChocolateyPackageVersion + '_PUBLIC'
+    $versionPath = 'ghidra_' + $env:ChocolateyPackageVersion -replace '\.\d{4}\d{2}\d{2}$' + '_PUBLIC'
 
     $toolsDir = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
     # Get path to ghidra dependency directory
