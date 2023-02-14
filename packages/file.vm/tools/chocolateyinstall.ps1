e0750aef0b555c23c3b06e16073efd67d5312e8d
thejoelpatrol <jcretan@gmail.com>
2023-02-14 10:05:38 -0500

    remove newline
    Co-authored-by: Moritz <mr-tz@users.noreply.github.com>

--- packages/file.vm/tools/chocolateyinstall.ps1
+++ packages/file.vm/tools/chocolateyinstall.ps1
@@ -9,7 +9,6 @@ try {
     $zipSha256 = "963147318f96d9345471e1a9a3943def4d95fcb3c1fe020e465ab910d0cda4a3"
 
     $executablePath = (VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true)[-1]
-    
     $executableDir = Split-Path -Path $executablePath
     $scriptPath = Join-Path $executableDir "leave_file_open.bat"
     [IO.File]::WriteAllLines($scriptPath, $("`"$executablePath`" %1", "PAUSE"))
