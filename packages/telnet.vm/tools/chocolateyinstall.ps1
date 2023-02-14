bbbb7cef98183a070b2e65945d9d6c2e0807973b
glitvinov <george.litvinov@mandiant.com>
2023-02-14 17:46:08 -0500

    create telnet package

---
+++ packages/telnet.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = 'telnet'
+  $category = 'Networking'
+
+  $system32=Join-Path $Env:WINDIR system32
+  $pkgmgr= Join-Path $system32 "pkgmgr.exe"
+  $rc = (Start-Process -FilePath $pkgmgr -Argumentlist '/iu:"TelnetClient"' -PassThru -Wait).ExitCode
+  if ($rc -ne 0) {
+    throw "PkgMgr returned error code $rc"
+  } else {
+    VM-Assert-Path $(Join-Path $system32 "telnet.exe")
+  }
+
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+  $executablePath = Join-Path $system32 "telnet.exe" -Resolve
+  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
+  VM-Assert-Path $shortcut
+} catch {
+  VM-Write-Log-Exception $_
+}
