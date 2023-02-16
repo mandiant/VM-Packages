e5d538292f2551523968e16b93be1b2580c14ce0
glitvinov <george.litvinov@mandiant.com>
2023-02-16 16:29:01 -0500

    added telnet package

---
+++ packages/telnet.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = 'telnet'
+  $category = 'Networking'
+
+  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+  $executablePath = Join-Path $([System.Environment]::SystemDirectory) telnet.exe -Resolve
+  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
+  VM-Assert-Path $shortcut
+} catch {
+  VM-Write-Log-Exception $_
+}
