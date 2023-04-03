7aa6ef837949d36320df1cad37fd333464861607
Menn1s <dennisqvt@protonmail.com>
2023-04-02 22:29:15 -0700

    added try catches

--- packages/mimikatz.vm/tools/chocolateyuninstall.ps1
+++ packages/mimikatz.vm/tools/chocolateyuninstall.ps1
@@ -1,15 +1,21 @@
-$ErrorActionPreference = 'Stop'
-$name = "Mimikatz"
-$path = Join-Path ${Env:TOOL_LIST_DIR} 'Password Attacks'
+try {
+    $ErrorActionPreference = 'Stop'
+    $name = "Mimikatz"
+    $path = Join-Path ${Env:TOOL_LIST_DIR} 'Password Attacks'
 
-$toolsDir = Join-Path "${Env:RAW_TOOLS_DIR}" $name
-Remove-Item -Path $toolsDir -ErrorAction SilentlyContinue -Recurse -Force
+    $toolsDir = Join-Path "${Env:RAW_TOOLS_DIR}" $name
+    Remove-Item -Path $toolsDir -ErrorAction SilentlyContinue -Recurse -Force
 
-$shortcut = Join-Path $path "mimikatz.x86.lnk"
-Remove-Item -Force $shortcut
-Uninstall-BinFile -Name "mimikatz.x86"
+    $shortcut = Join-Path $path "mimikatz.x86.lnk"
+    Remove-Item -Force $shortcut
+    Uninstall-BinFile -Name "mimikatz.x86"
 
 
-$shortcut = Join-Path $path "mimikatz.x64.lnk"
-Remove-Item -Force $shortcut
-Uninstall-BinFile -Name "mimikatz.x64"
+    $shortcut = Join-Path $path "mimikatz.x64.lnk"
+    Remove-Item -Force $shortcut
+    Uninstall-BinFile -Name "mimikatz.x64"
+}
+catch{
+    Write-Host "Mimikatz failed to uninstall:"
+    Write-Host $_
+}
