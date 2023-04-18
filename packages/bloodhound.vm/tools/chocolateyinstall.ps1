9a46a0cb8b0a4679fb5eeef315d109c878819986
vm-packages <vm-packages@mandiant.com>
2023-04-18 15:11:09 +0000

    Update bloodhound.vm to 4.3.0

--- packages/bloodhound.vm/tools/chocolateyinstall.ps1
+++ packages/bloodhound.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'BloodHound'
 $category = 'Information Gathering'
 
-$zipUrl = "https://github.com/BloodHoundAD/BloodHound/releases/download/4.2.0/BloodHound-win32-ia32.zip"
-$zipSha256 = "278934b8bf08d452d343a4da60e7453aba6bee5b20a7d91eaaac571131a4b623"
+$zipUrl = "https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.0/BloodHound-win32-ia32.zip"
+$zipSha256 = "52ce1c2667bd54eafb7485098eb77558883d8f0c091ff89faf761e722f5e9d45"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
