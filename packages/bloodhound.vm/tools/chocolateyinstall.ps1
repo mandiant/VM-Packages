ab5af66a362f6cb4bde0c535853b002cbb13a81a
Moritz <mr-tz@users.noreply.github.com>
2023-04-19 10:12:40 +0200

    Update packages/bloodhound.vm/tools/chocolateyinstall.ps1

--- packages/bloodhound.vm/tools/chocolateyinstall.ps1
+++ packages/bloodhound.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'BloodHound'
 $category = 'Information Gathering'
 
 $zipUrl = "https://github.com/BloodHoundAD/BloodHound/releases/download/v4.3.0/BloodHound-win32-ia32.zip"
-$zipSha256 = "52ce1c2667bd54eafb7485098eb77558883d8f0c091ff89faf761e722f5e9d45"
+$zipSha256 = "88e13a123b49e19188a4d51b83b060fd5fe0d6b4cdd96e6557792d229d339f64"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
