f7f70b7ab1a8177190e34a1bdafab6181f6d0fd2
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-04-21 20:09:03 +0200

    Fix pestudio.vm
    pestudio uses a download url that does not include the version and
    breaks with every update. Update to the last version to fix the hash
    mismatch.

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'pestudio'
 $category = 'PE'
 
 $zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
-$zipSha256 = 'E1CA8E305C080EF6880173BD7F17F7E4773C420F8545DB9A72F6958F1AEDDC0A'
+$zipSha256 = '2069D3FF8BA5B523D4CCC76A8C8191A1510F04DAD75EA23E35EB0D14F8C97C07'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
