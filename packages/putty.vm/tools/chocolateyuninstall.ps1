c85dc717792e533cf1d779fc354325f0f98dac55
skollr34p3r <48536777+skollr34p3r@users.noreply.github.com>
2023-05-01 11:01:00 -0400

    Revert "Merge pull request #1 from mandiant/main"
    This reverts commit 1d0f995970d69941f3780bc9642f6f85cd8b97cc, reversing
    changes made to 7299ed995c3716f944b19d5b7b6d892abdad8bef.

--- packages/putty.vm/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Continue'
-Import-Module vm.common -Force -DisableNameChecking
-
-$toolName = 'PuTTY'
-$category = 'Networking'
-
-VM-Remove-Tool-Shortcut $toolName $category
--- packages/shellcode_launcher.vm/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Continue'
-Import-Module vm.common -Force -DisableNameChecking
-
-$toolName = 'shellcode_launcher'
-$category = 'Utilities'
-
-VM-Uninstall $toolName $category
