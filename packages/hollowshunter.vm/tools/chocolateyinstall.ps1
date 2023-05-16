68dd08881e48cc39a8197e8fc4af5ded7caf27d9
vm-packages <vm-packages@mandiant.com>
2023-05-16 12:48:56 +0000

    Update hollowshunter.vm to 0.3.6

--- packages/hollowshunter.vm/tools/chocolateyinstall.ps1
+++ packages/hollowshunter.vm/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'hollows_hunter'
 $category = 'PE'
 
-$zipUrl = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.3.5/hollows_hunter32.zip'
-$zipSha256 = '0bbb0b794ac0b1c179a993a7bd26489fc1b8497aca05fa797b49f3d9bc71a6ca'
-$zipUrl_64 = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.3.5/hollows_hunter64.zip'
-$zipSha256_64 = '4ab368a12b761f4b351d0f9d60d4a1a1f80e6885025c798b4a972ab106c2d162'
+$zipUrl = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.3.6/hollows_hunter32.zip'
+$zipSha256 = 'b7a34de50fa8b7b289496c25cd96ecb79ca11dcc4a53586fdcd1a69ea32cb417'
+$zipUrl_64 = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.3.6/hollows_hunter64.zip'
+$zipSha256_64 = '3b5eadd70b44857db57328da8792e29d5b09e3e8561e609f389dd42b7747ef47'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -zipUrl_64 $zipUrl_64 -zipSha256_64 $zipSha256_64 -consoleApp $true
 
