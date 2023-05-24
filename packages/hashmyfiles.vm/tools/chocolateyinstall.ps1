4ff1b61e2e9b6429250a7abd183e673850c1c0a5
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-05-24 17:02:39 +0200

    hashmyfiles: Fix right click menu command
    The previous command opened HashMyFiles but without the file/directory.

--- packages/hashmyfiles.vm/tools/chocolateyinstall.ps1
+++ packages/hashmyfiles.vm/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ try {
   $zipUrl_64 = "https://www.nirsoft.net/utils/hashmyfiles-x64.zip"
 
   $executablePath = (VM-Install-From-Zip $toolName $category $zipUrl -zipUrl_64 $zipUrl_64)[-1]
-  VM-Add-To-Right-Click-Menu $toolName "HashMyFiles" "`"$executablePath`" `"%1`"" "file" "$executablePath"
-  VM-Add-To-Right-Click-Menu $toolName "HashMyFiles" "`"$executablePath`" `"%1`"" "directory" "$executablePath"
+  VM-Add-To-Right-Click-Menu $toolName "HashMyFiles" "`"$executablePath`" /file `"%1`"" "file" "$executablePath"
+  VM-Add-To-Right-Click-Menu $toolName "HashMyFiles" "`"$executablePath`" /file `"%1`"" "directory" "$executablePath"
 } catch {
   VM-Write-Log-Exception $_
 }
