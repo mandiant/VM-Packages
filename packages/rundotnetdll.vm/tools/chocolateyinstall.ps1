a1e345871b7b2de7423e6ebacd4a59141e01c162
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-05-26 11:13:28 +0200

    rundotnetdll.vm: Fix shortcut
    RunDotNetDll does not have a `--help` parameter and the output is
    confusing.

--- packages/rundotnetdll.vm/tools/chocolateyinstall.ps1
+++ packages/rundotnetdll.vm/tools/chocolateyinstall.ps1
@@ -7,5 +7,5 @@ $category = 'dotNet'
 $zipUrl = 'https://github.com/enkomio/RunDotNetDll/releases/download/2.2/RunDotNetDll.zip'
 $zipSha256 = '27B922861DD27C8DC484524EB7B3AE8F2FB6CA44C1C7086D9ED529A7B4E7CC1D'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -arguments $null
 
