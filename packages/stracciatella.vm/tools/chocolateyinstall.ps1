01da2f11b8439214597e9f2bb607c2ef1e52a780
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-05-29 17:36:11 -0700

    update download url and hash

--- packages/stracciatella.vm/tools/chocolateyinstall.ps1
+++ packages/stracciatella.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Stracciatella'
 $category = 'Command & Control'
 
-$zipUrl = 'https://github.com/mgeeky/Stracciatella/archive/refs/heads/master.zip'
-$zipSha256 = 'db6f32b336dc02aa1dd0577a10348c1ce5558d144f889e63cacff8a9612df2a3'
+$zipUrl = 'https://github.com/mgeeky/Stracciatella/archive/acc83e21951049ab4998ecd18f5e4fa01e1527da.zip'
+$zipSha256 = 'd9299fca780945becf9907b052112e7149fb2a2d51e28f0e73e8326455f47a82'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
