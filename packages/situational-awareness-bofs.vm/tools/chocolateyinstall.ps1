f36f620f61ec0ab9220b67f6c69a68dd0f27a53e
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-06-01 21:19:50 -0700

    updated category

--- packages/situational-awareness-bofs.vm/tools/chocolateyinstall.ps1
+++ packages/situational-awareness-bofs.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'Situational Awareness BOF'
-$category = 'Command & Control'
+$category = 'Information Gathering'
 
 $zipUrl = 'https://github.com/trustedsec/CS-Situational-Awareness-BOF/archive/82235696478ec49fc10bc09a11483d75dddd0a45.zip'
 $zipSha256 = '4f32d34529e6f7b6b287bd61a768c1e56345de8e563da7d4b15661552d8a5037'
