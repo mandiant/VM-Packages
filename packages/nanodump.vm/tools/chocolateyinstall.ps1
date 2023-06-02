46a96e2c881d185c604a6dee232299e451f3c013
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-06-01 21:25:49 -0700

    updated category

--- packages/nanodump.vm/tools/chocolateyinstall.ps1
+++ packages/nanodump.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'NanoDump'
-$category = 'Command & Control'
+$category = 'Password Attacks'
 
 $zipUrl = 'https://github.com/fortra/nanodump/archive/c211c5f72b2438afb09d0eb917fe32150be91344.zip'
 $zipSha256 = '461a16ae517aebb65adc37a0da8f8c04fa4836da35a69239dc2f90f8098b5da0'
