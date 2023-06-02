bdbdbf96e901e2b8bb9141903919f231c0bc436f
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-06-01 21:24:35 -0700

    updated category

--- packages/stracciatella.vm/tools/chocolateyuninstall.ps1
+++ packages/stracciatella.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'Stracciatella'
-$category = 'Command & Control'
+$category = 'Evasion'
 
 VM-Uninstall $toolName $category
