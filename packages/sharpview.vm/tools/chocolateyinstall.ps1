f4d1ff84a70f88b982b19b0f9dd92df7e3b79380
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-06-02 13:30:41 -0700

    Update hash

--- packages/sharpview.vm/tools/chocolateyinstall.ps1
+++ packages/sharpview.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'SharpView'
 $category = 'Information Gathering'
 
 $zipUrl = 'https://github.com/tevora-threat/SharpView/archive/b60456286b41bb055ee7bc2a14d645410cca9b74.zip'
-$zipSha256 = '1e5f54b9317ac053fe51e373b3e3b830573e2d14612bf4a038750a6c6284fb3d'
+$zipSha256 = 'b5b2dd91fe22f56fb846d849052fc3205f177cbd067069e6d829e38eea0aca49'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
