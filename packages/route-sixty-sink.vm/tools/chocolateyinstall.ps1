e6364604ff899fde4b25dd7bfb6bf6c3c3d57505
vm-packages <vm-packages@mandiant.com>
2023-06-02 05:01:40 +0000

    Add route-sixty-sink.vm
    Closes https://github.com/mandiant/VM-Packages/issues/387.

---
+++ packages/route-sixty-sink.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'RouteSixtySink'
+$category = 'Web Application'
+
+$zipUrl = 'https://github.com/mandiant/route-sixty-sink/releases/download/v1.0.0/route-sixty-sink-win-x64.zip'
+$zipSha256 = 'deef7a27d7cfce6d2d0fdab6e9cd5d9a0527a58538d8dcc8801de53cddeba876'
+
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
