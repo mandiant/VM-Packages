09c813c1dbf29d9c3f8a2c3dcb248c31191c23bc
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-06-01 21:33:11 -0700

    fixed package name and category

--- packages/trustedsec-remote-ops-bof.vm/tools/chocolateyuninstall.ps1
+++ packages/trustedsec-remote-ops-bof.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'Remote Operations BOF'
-$category = 'Command & Control'
+$category = 'Information Gathering'
 
 VM-Uninstall $toolName $category
