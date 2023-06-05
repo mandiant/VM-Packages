79c08f597f95230dae84d822dcc02c2842688fb4
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-06-05 15:10:49 +0200

    dnspyex.vm: Fix dnSpy
    Install dnSpyEx directly instead of using the community package as it
    does not install both 64 and 32 versions. Add shortcuts for both GUI
    applications (in addition to the console app that was already created).

--- packages/dnspyex.vm/tools/chocolateyinstall.ps1
+++ packages/dnspyex.vm/tools/chocolateyinstall.ps1
@@ -2,12 +2,22 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  $toolName = 'dnSpyEx'
+  $toolName = 'dnSpy'
   $category = 'dotNet'
-  $shimPath = 'bin\dnSpy.Console.exe'
 
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
-  VM-Install-Shortcut $toolName $category $executablePath -consoleApp $true -arguments $null
+  $zipUrl = "https://github.com/dnSpyEx/dnSpy/releases/download/v6.3.0/dnSpy-netframework.zip"
+  $zipSha256 = "122df37b8668eb38be1c139cb244185824ff0f2ab4b4c81862a8397c6c2e7f1f"
+  $toolDir = (VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256)[0]
+
+  $toolNameX86 = "$toolName-x86"
+  $executablePathX86 = Join-Path $toolDir "$toolNameX86.exe" -Resolve
+  VM-Install-Shortcut $toolNameX86 $category $executablePathX86
+  Install-BinFile -Name $toolNameX86 -Path $executablePathX86
+
+  $toolNameConsole = "$toolName.Console"
+  $executablePathConsole = Join-Path $toolDir "$toolNameConsole.exe" -Resolve
+  VM-Install-Shortcut $toolNameConsole $category $executablePathConsole -consoleApp $true -arguments $null
+  Install-BinFile -Name $toolNameConsole -Path $executablePathConsole
 } catch {
   VM-Write-Log-Exception $_
 }
