8f0a39ca7655effb87300e0382265b4196a4874a
vm-packages <vm-packages@mandiant.com>
2023-06-06 09:19:25 +0000

    Update floss.vm to 2.3.0

--- packages/floss.vm/tools/chocolateyinstall.ps1
+++ packages/floss.vm/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'FLOSS'
 $category = 'Utilities'
 
-$zipUrl = "https://github.com/mandiant/flare-floss/releases/download/v2.2.0/floss-v2.2.0-windows.zip"
-$zipSha256 = "edc206110a62bba4c27ff245d93e66d237c74c27f98ae05b9478151fbaed8aee"
+$zipUrl = "https://github.com/mandiant/flare-floss/releases/download/v2.3.0/floss-v2.3.0-windows.zip"
+$zipSha256 = "30afca951815545e68c76feae0675127fe742e5b3b16f75ba751aef3985ab053"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
 
