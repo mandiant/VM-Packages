691b1e2ad23918e2f9fd9ecffdac39c18537a3e7
skollr34p3r <48536777+skollr34p3r@users.noreply.github.com>
2023-06-07 10:05:36 -0400

    updating heidisql

--- packages/heidisql.vm/tools/chocolateyinstall.ps1
+++ packages/heidisql.vm/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@ Import-Module vm.common -Force -DisableNameChecking
 
 try{
     $toolName = 'HeidiSQL'
-    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
-    $toolsDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
     $category = 'Utilities'
+    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+    $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
 
     $exeUrl = 'https://www.heidisql.com/installers/HeidiSQL_12.4.0.6659_Setup.exe'
     $exeSha256 = '205da1014259b36891981b1044f5429412b9a3050dfc2e2864b9fd724200f227'
@@ -24,21 +24,19 @@ try{
 
     # AutoHotkey the installer
     $ahkInstaller = Join-Path ${Env:TEMP} $installerName -Resolve
-    $rc = (Start-Process -FilePath $ahkInstaller -ArgumentList '/VERYSILENT', '/SUPPRESSMSGBOXES', '/DIR="C:\Tools\HeidiSQL"' -PassThru -Wait).ExitCode
+    $rc = (Start-Process -FilePath $ahkInstaller -ArgumentList '/VERYSILENT /SUPPRESSMSGBOXES /DIR="C:\Tools\HeidiSQL" /MERGETASKS="!desktopicon,!install_snippets"' -PassThru -Wait).ExitCode
     if ($rc -eq 1) {
         throw "AutoHotKey returned a failure exit code ($rc) for: ${Env:ChocolateyPackageName}"
     } else {
-        VM-Assert-Path $(Join-Path $toolsDir "HeidiSQL.exe")
-        # Remove desktop shortcut
-        Get-ChildItem 'C:\Users\Public\Desktop\HeidiSQL.lnk' | ForEach-Object { Remove-Item $_ }
+        VM-Assert-Path $(Join-Path $toolDir "HeidiSQL.exe")
     }
 
     # Add shortcuts for HeidiSQL
-    $executablePath = Join-Path $toolsDir "HeidiSQL.exe" -Resolve
-    $shortcut = Join-Path $shortcutDir "HeidiSQL.lnk"
+    $executablePath = Join-Path $toolDir "HeidiSQL.exe" -Resolve
+    $shortcut = Join-Path $shortcutDir "$toolName.lnk"
     Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
     VM-Assert-Path $shortcut
-    Install-BinFile -Name "HeidiSQL" -Path $executablePath
+    #Install-BinFile -Name "HeidiSQL" -Path $executablePath
 
 } catch {
     VM-Write-Log-Exception $_
