1a455514c0efacdaf878e92a39363aa5a2a8f438
Ana María Martínez Gómez <ana.martinez@mandiant.com>
2023-06-23 15:59:16 +0200

    upx.vm: Add innerFolder argument
    The zip contains a folder with the EXE and other needed files.

--- packages/upx.vm/tools/chocolateyinstall.ps1
+++ packages/upx.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $category = 'Utilities'
 $zipUrl = 'https://github.com/upx/upx/releases/download/v4.0.2/upx-4.0.2-win64.zip'
 $zipSha256 = '325c58ea2ed375afbd4eeac0b26f15f98db0d75dea701205ca10d8bf4d2fdc24'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $true
