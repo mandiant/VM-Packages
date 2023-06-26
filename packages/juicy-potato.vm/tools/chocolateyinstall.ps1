6b1ca1494e8d39d0626ccb9dd21d796b82570403
vm-packages <vm-packages@mandiant.com>
2023-06-26 20:27:16 +0000

    Add juicy-potato.vm
    Closes https://github.com/mandiant/VM-Packages/issues/450.

---
+++ packages/juicy-potato.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'JuicyPotato'
+$category = 'Exploitation'
+
+$exeUrl = 'https://github.com/ohpe/juicy-potato/releases/download/v0.1/JuicyPotato.exe'
+$exeSha256 = '0f56c703e9b7ddeb90646927bac05a5c6d95308c8e13b88e5d4f4b572423e036'
+
+VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
