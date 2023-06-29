2f1f2b711b55cf016fd7015c40111efb344d53d8
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-06-29 11:59:27 +0200

    pestudio: Use new link that includes version
    pestudio uses now a link that includes the version and will exist for
    two months, giving us time to update without breaking the package.

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'pestudio'
 $category = 'PE'
 
-$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
+$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio-9.53.zip'
 $zipSha256 = 'ded56a58d7c5e06453f22a875d6a58ec0c07031e1c0873acc06b288a2d9658d8'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
