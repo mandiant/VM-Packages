4a0aa151a37da3d5b5fa6dc6e127d3c4cf979ed1
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 12:10:44 -0700

    manually importing modules to avoid errors with the shortcut

--- packages/microburst.vm/tools/chocolateyinstall.ps1
+++ packages/microburst.vm/tools/chocolateyinstall.ps1
@@ -7,6 +7,6 @@ $category = 'Cloud'
 $zipUrl = 'https://github.com/NetSPI/MicroBurst/archive/10a4f9579de3fe687208f7a3d3d1120fbba408cc.zip'
 $zipSha256 = '72700519c40fac2b01e5362e4d3d1e171e73910f8e9e9859753f26c64f0529d0'
 
-$powershellCommand = 'Import-Module .\MicroBurst.psm1; Get-Help Get-AzDomainInfo'
+$powershellCommand = 'Import-Module AzureAD $PSScriptRoot\AzureAD\MicroBurst-AzureAD.psm1 $PSScriptRoot\Misc\MicroBurst-Misc.psm1 $PSScriptRoot\REST\MicroBurst-AzureREST.psm1'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
