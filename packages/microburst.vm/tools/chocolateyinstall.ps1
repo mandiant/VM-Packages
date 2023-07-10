aadbc459dfbea88a9ca41fd132b9dc7b2d6f1558
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 11:09:29 -0700

    removed trailing whitespace

--- packages/microburst.vm/tools/chocolateyinstall.ps1
+++ packages/microburst.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,4 @@ $category = 'Cloud'
 $zipUrl = 'https://github.com/NetSPI/MicroBurst/archive/10a4f9579de3fe687208f7a3d3d1120fbba408cc.zip'
 $zipSha256 = '72700519c40fac2b01e5362e4d3d1e171e73910f8e9e9859753f26c64f0529d0'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 $powershellCommand = 'Import-Module .\MicroBurst.psm1' 
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 $powershellCommand = 'Import-Module .\MicroBurst.psm1'
