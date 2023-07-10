b4176900e4c4af3e49dfb28319eb809d9e7349dd
vm-packages <vm-packages@mandiant.com>
2023-07-10 21:36:16 +0000

    Add invokedosfuscation.vm
    Closes https://github.com/mandiant/VM-Packages/issues/489.

---
+++ packages/invokedosfuscation.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'Invoke-DOSfuscation'
+$category = 'Payload Development'
+
+$zipUrl = 'https://github.com/danielbohannon/Invoke-DOSfuscation/archive/6260f5b5848b967446371ee7800aaa0409ea23cc.zip'
+$zipSha256 = '60b78094731fc8f54333193e840cb847ac4018c6ca1ccc36c107cda533016791'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
