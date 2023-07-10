0455b66bafdba68a45c454ca0c3fb460cf993dd4
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 14:08:25 -0700

    added a shortcut command

--- packages/powerupsql.vm/tools/chocolateyinstall.ps1
+++ packages/powerupsql.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Exploitation'
 $zipUrl = 'https://github.com/NetSPI/PowerUpSQL/archive/2837c7bdda47a07703b7841080024f30a73a7743.zip'
 $zipSha256 = 'fffed1c3f480b40616070e7ebb5bf7e8093e0bb483ce1ef2400f586018439c85'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
+$powershellCommand = 'Import-Module PowerUpSQL.psd1; Get-Command -Module PowerUpSQL'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
