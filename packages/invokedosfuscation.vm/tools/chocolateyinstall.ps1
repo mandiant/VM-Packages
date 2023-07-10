e8f059f8bb50a5f249705ab70ea64100c74bc20f
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 14:37:28 -0700

    added a shortcut command

--- packages/invokedosfuscation.vm/tools/chocolateyinstall.ps1
+++ packages/invokedosfuscation.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Payload Development'
 $zipUrl = 'https://github.com/danielbohannon/Invoke-DOSfuscation/archive/6260f5b5848b967446371ee7800aaa0409ea23cc.zip'
 $zipSha256 = '60b78094731fc8f54333193e840cb847ac4018c6ca1ccc36c107cda533016791'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
+$powershellCommand = 'Import-Module .\Invoke-DOSfuscation.psd1; Invoke-DOSfuscation'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
