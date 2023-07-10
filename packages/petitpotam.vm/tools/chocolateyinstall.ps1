f2ff310482fe098fd8b88b4cba35c55d4ff43ff2
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 16:37:20 -0700

    added shortcut

--- packages/petitpotam.vm/tools/chocolateyinstall.ps1
+++ packages/petitpotam.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Exploitation'
 $zipUrl = 'https://github.com/topotam/PetitPotam/archive/d83ac8f2dd34654628c17490f99106eb128e7d1e.zip'
 $zipSha256 = '5429479879877c2a6263d79c1a83fbcbd0f9f37bf9870c155358d9dc25662862'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
+$powershellCommand = '.\PetitPotam.exe'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
