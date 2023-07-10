943283c6079b09284836aa46ac8caf9727c02017
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 13:57:58 -0700

    added shortcut command

--- packages/invokeobfuscation.vm/tools/chocolateyinstall.ps1
+++ packages/invokeobfuscation.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Payload Development'
 $zipUrl = 'https://github.com/danielbohannon/Invoke-Obfuscation/archive/f20e7f843edd0a3a7716736e9eddfa423395dd26.zip'
 $zipSha256 = '24149efe341b4bfc216dea22ece4918abcbe0655d3d1f3c07d1965fac5b4478e'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
+$powershellCommand = 'Import-Module ./Invoke-Obfuscation.psd1; Invoke-Obfuscation'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
