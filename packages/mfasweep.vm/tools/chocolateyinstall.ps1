d0e7ca3552179e9129ca1fc3bae2a36a8e5d83a2
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 14:46:47 -0700

    added shortcut

--- packages/mfasweep.vm/tools/chocolateyinstall.ps1
+++ packages/mfasweep.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Credential Access'
 $ps1Url = 'https://raw.githubusercontent.com/dafthack/MFASweep/d624cdedb3eaa40518990b234b308c1ec8f6bbc7/MFASweep.ps1'
 $ps1Sha256 = 'e5df6d6914a197455d4779c50863f86c5086abbcb6638a4d9ea50598313c1481'
 
-VM-Install-Single-Ps1 $toolName $category $ps1Url -ps1Sha256 $ps1Sha256
+$powershellCommand = 'Import-Module .\MFASweep.ps1; Get-Help Invoke-MFASweep'
+
+VM-Install-Single-Ps1 $toolName $category $ps1Url -ps1Sha256 $ps1Sha256 -powershellCommand $powershellCommand
