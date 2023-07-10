4570854c9b8f2a41cd96217f1ed7ad2bc672d7be
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 12:28:21 -0700

    fixed shortcut, changed category

--- packages/microburst.vm/tools/chocolateyinstall.ps1
+++ packages/microburst.vm/tools/chocolateyinstall.ps1
@@ -2,11 +2,11 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'MicroBurst'
-$category = 'Cloud'
+$category = 'Exploitation'
 
 $zipUrl = 'https://github.com/NetSPI/MicroBurst/archive/10a4f9579de3fe687208f7a3d3d1120fbba408cc.zip'
 $zipSha256 = '72700519c40fac2b01e5362e4d3d1e171e73910f8e9e9859753f26c64f0529d0'
 
-$powershellCommand = 'Import-Module AzureAD $PSScriptRoot\AzureAD\MicroBurst-AzureAD.psm1 $PSScriptRoot\Misc\MicroBurst-Misc.psm1 $PSScriptRoot\REST\MicroBurst-AzureREST.psm1'
+$powershellCommand = 'Import-Module Az, .\Az\MicroBurst-Az.psm1, .\Misc\MicroBurst-Misc.psm1, .\REST\MicroBurst-AzureREST.psm1'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
