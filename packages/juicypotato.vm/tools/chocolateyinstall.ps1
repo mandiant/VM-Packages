98dc126aaf8cbd0229b61624c340214c3424b55c
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 13:43:39 -0700

    renamed. changed category.

--- packages/juicypotato.vm/tools/chocolateyinstall.ps1
+++ packages/juicypotato.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'JuicyPotato'
-$category = 'Exploitation'
+$category = 'Privilege Escalation'
 
 $exeUrl = 'https://github.com/ohpe/juicy-potato/releases/download/v0.1/JuicyPotato.exe'
 $exeSha256 = '0f56c703e9b7ddeb90646927bac05a5c6d95308c8e13b88e5d4f4b572423e036'
