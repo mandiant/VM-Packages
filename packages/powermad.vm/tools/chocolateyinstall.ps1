ff2c03a0db74cfc1d9768bb9f62e24711a6e83e7
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 18:24:25 -0700

    added shortcut command

--- packages/powermad.vm/tools/chocolateyinstall.ps1
+++ packages/powermad.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Android'
 $zipUrl = 'https://github.com/Kevin-Robertson/Powermad/archive/3ad36e655d0dbe89941515cdb67a3fd518133dcb.zip'
 $zipSha256 = 'e01cfdb69f938ecd8c707e81dce2832935bb26e368405f2180b6858bce5b4d73'
 
-VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
+$powershellCommand = 'Import-Module .\Powermad.psd1; Get-Command -Module Powermad'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
