2a7ab743fd6f798de89c9e8a3a6227d3d54d47d0
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-10 18:25:02 -0700

    fixed category

--- packages/powermad.vm/tools/chocolateyuninstall.ps1
+++ packages/powermad.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'PowerMad'
-$category = 'Android'
+$category = 'Exploitation'
 
 VM-Uninstall $toolName $category
