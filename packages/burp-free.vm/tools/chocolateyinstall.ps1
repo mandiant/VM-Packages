02d9ef9f71208eb22047e0b178689442cd8729f9
unknown <ephness@gmail.com>
2023-07-11 11:23:27 -0700

    fix burp-free.vm install

--- packages/burp-free.vm/tools/chocolateyinstall.ps1
+++ packages/burp-free.vm/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'BurpSuiteCommunity'
   $category = 'Utilities'
-  $shimPath = '"C:\Program Files\BurpSuiteCommunity\BurpSuiteCommunity.exe"'
+  $shimPath = 'BurpSuiteCommunity\BurpSuiteCommunity.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  $executablePath = Join-Path ${Env:ProgramFiles} $shimPath -Resolve
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
 } catch {
