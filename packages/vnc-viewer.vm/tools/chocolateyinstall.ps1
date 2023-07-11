9ed76a448a8620400c1f5cfddf36af254838fa4a
Menn1s <38476987+Menn1s@users.noreply.github.com>
2023-07-11 10:16:14 -0700

    Update chocolateyinstall.ps1
    Using executable path instead of specifying shim. The chocolatey install template assumes a shim is created

--- packages/vnc-viewer.vm/tools/chocolateyinstall.ps1
+++ packages/vnc-viewer.vm/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'VNC-Viewer'
   $category = 'Utilities'
-  $shimPath = 'C:\Program Files\RealVNC\VNC Viewer\vncviewer.exe'
+ 
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  $executablePath = Join-Path ${Env:ProgramFiles} 'RealVNC\VNC Viewer\vncviewer.exe' -Resolve
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
 } catch {
