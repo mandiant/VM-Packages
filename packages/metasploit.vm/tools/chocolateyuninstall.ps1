b7248c953d32f6e6668c4518b03c50ea2d0c46fd
Menn1s <dennisqvt@protonmail.com>
2023-07-13 16:33:05 -0700

    added metasploit

---
+++ packages/metasploit.vm/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Continue'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'Metasploit'
+$category = 'Command & Control'
+
+VM-Uninstall $toolName $category
+
+
+# Silently uninstall
+VM-Uninstall-With-Uninstaller $toolName "MSI" "/q /norestart"
+
+# Remove directory, shortcut, shim
+VM-Uninstall $toolName $category
