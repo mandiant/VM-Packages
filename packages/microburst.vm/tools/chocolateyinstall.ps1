f9f17af9625fe6e3712b8d18dc5223c6df8e849b
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-07-13 10:45:08 -0700

    Add MicroBurst (#275)
    * added microburst
    
    * added a shortcut psm import command
    
    * removed trailing whitespace
    
    * added the az dependency
    
    * manually importing modules to avoid errors with the shortcut
    
    * fixed shortcut, changed category
    
    * added help menu to the shortcut
    
    * updating category for uninstall to match install
    
    ---------
    
    Co-authored-by: unknown <ephness@gmail.com>

---
+++ packages/microburst.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,12 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'MicroBurst'
+$category = 'Exploitation'
+
+$zipUrl = 'https://github.com/NetSPI/MicroBurst/archive/10a4f9579de3fe687208f7a3d3d1120fbba408cc.zip'
+$zipSha256 = '72700519c40fac2b01e5362e4d3d1e171e73910f8e9e9859753f26c64f0529d0'
+
+$powershellCommand = 'Import-Module Az, .\Az\MicroBurst-Az.psm1, .\Misc\MicroBurst-Misc.psm1, .\REST\MicroBurst-AzureREST.psm1; Get-Command -Module MicroBurst-Az'
+
+VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256 -powershellCommand $powershellCommand
