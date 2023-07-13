ba8cb538caa58091164b7346c47866be1dd3745e
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-12 17:36:54 -0700

    added shortcut

--- packages/getlapspasswords.vm/tools/chocolateyinstall.ps1
+++ packages/getlapspasswords.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Credential Access'
 $ps1Url = 'https://raw.githubusercontent.com/kfosaaen/Get-LAPSPasswords/2aeffed5dc16c0d7be91ba67b79cfaaf1da1eecd/Get-LAPSPasswords.ps1'
 $ps1Sha256 = '2256ef01ad1a82633abc376246f0d05784a26f16fc99f1b66e6d42a3fafa2eb4'
 
-VM-Install-Single-Ps1 $toolName $category $ps1Url -ps1Sha256 $ps1Sha256
+$ps1Cmd = 'Import-Module; Get-Help Get-LAPSPasswords'
+
+VM-Install-Single-Ps1 $toolName $category $ps1Url -ps1Sha256 $ps1Sha256 -ps1Cmd $ps1Cmd
