98ca264b3018a0768fb04c1e1975df87bf83c097
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-13 16:37:48 -0700

    added to path

--- packages/resourcehacker.vm/tools/chocolateyinstall.ps1
+++ packages/resourcehacker.vm/tools/chocolateyinstall.ps1
@@ -9,6 +9,7 @@ try {
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
   $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  Install-BinFile -Name $toolName -Path $executablePath
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
 } catch {
