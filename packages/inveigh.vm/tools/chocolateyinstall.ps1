3bc0a1c64ab53a3ae8e8c9f6c765db79919568be
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-13 15:53:23 -0700

    added arguments

--- packages/inveigh.vm/tools/chocolateyinstall.ps1
+++ packages/inveigh.vm/tools/chocolateyinstall.ps1
@@ -7,4 +7,6 @@ $category = 'Credential Access'
 $zipUrl = 'https://github.com/Kevin-Robertson/Inveigh/releases/download/v2.0.10/Inveigh-net7.0-v2.0.10.zip'
 $zipSha256 = '8c61ccdccc84f2223c5c3da2014deb79cf807c26db0b018373e776baa26537bc'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+$arguments = '-?'
+
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -arguments $arguments
