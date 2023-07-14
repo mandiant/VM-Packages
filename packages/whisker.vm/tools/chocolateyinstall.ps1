968f075d799d1180032927cc4e53ea6c243bb6f4
Alex | NoSecurity <72961537+nos3curity@users.noreply.github.com>
2023-07-14 11:10:02 -0700

    Whisker and RouteSixtySink Category Change (#591)
    * recategorized packages
    
    * fix capitalization in category variable
    
    * updated package versions
    
    * recategorized whisker and routesixtysink

--- packages/whisker.vm/tools/chocolateyinstall.ps1
+++ packages/whisker.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'Whisker'
-$category = 'Persistence'
+$category = 'Exploitation'
 
 $zipUrl = 'https://github.com/eladshamir/Whisker/archive/0bc2a0acc4a92b49c69d873f7ac565340a5f3291.zip'
 $zipSha256 = 'b181b639f2d18fb37e045d27cbe522e7b97aaa85c30dc0cb9bc75eaf6b939f9a'
