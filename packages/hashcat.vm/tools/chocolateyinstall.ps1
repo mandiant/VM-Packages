e4be8093891033916ce6f47d0ffa3c7ec96f35de
Menn1s <dennisqvt@protonmail.com>
2023-07-13 22:04:26 -0700

    add hashcat

--- packages/hashcat.vm/tools/chocolateyinstall.ps1
+++ packages/hashcat.vm/tools/chocolateyinstall.ps1
@@ -6,5 +6,25 @@ $category = 'Credential Access'
 
 $zipUrl = 'https://github.com/hashcat/hashcat/releases/download/v6.2.6/hashcat-6.2.6.7z'
 $zipSha256 = '96697e9ef6a795d45863c91d61be85a9f138596e3151e7c2cd63ccf48aaa8783'
+$zipName = 'hashcat-6.2.6'
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
+try {
+    # Download the zip file
+    $packageArgs = @{
+        packageName   = ${Env:ChocolateyPackageName}
+        url           = $zipUrl
+        checksum      = $zipSha256
+        checksumType  = "sha256"
+        fileFullPath  = Join-Path "${Env:USERPROFILE}\AppData\Local\Temp" ("$zipName.7z")
+    }
+    Get-ChocolateyWebFile @packageArgs
+    $zipPath = $packageArgs.fileFullPath
+    VM-Assert-Path $zipPath
+
+    7z x $zipPath -o$toolDir -y
+    # Create a shortcut
+    $executablePath = Join-Path ${Env:RAW_TOOLS_DIR} "$toolName\$zipName\$toolName.exe" -Resolve
+    VM-Install-Shortcut $toolName $category $executablePath
+} catch {
+    VM-Write-Log-Exception $_
+}
