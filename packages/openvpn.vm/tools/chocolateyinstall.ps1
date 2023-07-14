3a599fcb4c529c05a6f9dceb12dc7bf0bcc3f94c
nos3curity <72961537+nos3curity@users.noreply.github.com>
2023-07-13 21:07:31 -0700

    removing shortcut and autostartup

--- packages/openvpn.vm/tools/chocolateyinstall.ps1
+++ packages/openvpn.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  $toolName = 'OpenVPN'
+  $toolName = 'OpenVPN GUI'
   $category = 'Networking'
   $shimPath = 'C:\Program Files\OpenVPN\bin\openvpn-gui.exe'
 
@@ -11,6 +11,14 @@ try {
   $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
+
+  # Removing the shortcut
+  $desktopShortcut = Join-Path ${Env:Public} "Desktop\$toolName.lnk"
+  Remove-Item $desktopShortcut -Force -ea 0
+
+  # Removing OpenVPN from startup
+  Remove-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' -Name 'OpenVPN-GUI' -ErrorAction SilentlyContinue
+
 } catch {
   VM-Write-Log-Exception $_
 }
