193a55b814d36bfc438e15f9f63741f8a1948c36
unknown <ephness@gmail.com>
2023-07-14 10:31:15 -0400

    fix shimpath and executablepath

--- packages/heidisql.vm/tools/chocolateyinstall.ps1
+++ packages/heidisql.vm/tools/chocolateyinstall.ps1
@@ -4,11 +4,11 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'heidisql'
   $category = 'Utilities'
-  $shimPath = 'Join-Path ${Env:ProgramFiles} "\HeidiSQL\heidisql.exe"'
+  $shimPath = '\HeidiSQL\heidisql.exe'
 
   $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
   $shortcut = Join-Path $shortcutDir "$toolName.lnk"
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
+  $executablePath = Join-Path ${Env:ProgramFiles} $shimPath -Resolve
   Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath -RunAsAdmin
   VM-Assert-Path $shortcut
 } catch {
