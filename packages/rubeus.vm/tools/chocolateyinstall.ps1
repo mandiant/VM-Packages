fa20fe831ed37aeae159395ecf22f87db1f36792
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2023-08-11 03:13:26 -0400

    Rubeus patch for resolving SHA256 mismatch on 2.2.3 release (#629)
    * Update rubeus.vm.nuspec
    
    Updating to 2.2.3 per version posted in latest build at Rubeus/Domain/Info.cs
    
    * Update chocolateyinstall.ps1
    
    Update SHA256

--- packages/rubeus.vm/tools/chocolateyinstall.ps1
+++ packages/rubeus.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'Rubeus'
 $category = 'Credential Access'
 
 $zipUrl = 'https://github.com/GhostPack/Rubeus/archive/refs/heads/master.zip'
-$zipSha256 = 'f6d1650043e528e24b4bc5c1f24e64ff0c4bbcf72537b84b1d8f866dd8ab8ccb'
+$zipSha256 = 'DC61768AF588A5FCC1CEDC491E8DF81BC652A96A1A032741034E40B75EC404F2'
 
 VM-Install-Raw-GitHub-Repo $toolName $category $zipUrl $zipSha256
