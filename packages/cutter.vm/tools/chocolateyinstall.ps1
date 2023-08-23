81dd79d358dd71f244e2d28a76b060a6f9a1a7b5
Moritz <mr-tz@users.noreply.github.com>
2023-08-23 10:42:59 +0200

    Revert ":robot: Package update"

--- packages/cutter.vm/tools/chocolateyinstall.ps1
+++ packages/cutter.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Cutter'
 $category = 'Disassemblers'
 
-$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.3.1/Cutter-v2.3.1-Windows-x86_64.zip'
-$zipSha256 = '52939105aff4f5b5ce82e17ddcd62e9cfb3c14684c1cbf98e6f485af1e47b074'
+$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.3.0/Cutter-v2.3.0-Windows-x86_64.zip'
+$zipSha256 = 'a708f0884421e7a90e95e5389697931ce5b282f8dfe0219b206b4837071bd770'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
