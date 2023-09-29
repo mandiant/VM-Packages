8239d7c3c00d2e86fd25cd9b709818b39d96f846
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-09-29 11:40:16 +0200

    Introduce Packers category
    Introduce Packers category as the Utilities folder is getting big and to
    make easier finding tools. Move upx to the Packers category. This change
    likely breaks updates.

--- packages/upx.vm/tools/chocolateyuninstall.ps1
+++ packages/upx.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'upx'
-$category = 'Utilities'
+$category = 'Packers'
 
 VM-Uninstall $toolName $category
