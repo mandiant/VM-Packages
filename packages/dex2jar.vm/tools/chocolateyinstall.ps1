066080c4c757992bd74fbb5d094bac190ccf81fc
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-10-23 10:13:06 +0200

    category: Rename 'Android/Java' to fix bug
    The `Java/Android` category is creating a subfolder `Android` inside of
    the `Java` category and this is not the desired result. Rename to `Java
    & Android` to fix the issue.

--- packages/dex2jar.vm/tools/chocolateyinstall.ps1
+++ packages/dex2jar.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'd2j-dex2jar'
-$category = 'Java/Android'
+$category = 'Java & Android'
 
 $zipUrl = 'https://github.com/pxb1988/dex2jar/releases/download/v2.3/dex2jar-v2.zip'
 $zipSha256 = 'd0507b6277193476ae29351905b5fa9b20d1a9a5ce119b46d87e5b188edf859e'
