3cac4aaf84509448ca2d8c2d8bcfbda754d10362
vm-packages <vm-packages@mandiant.com>
2023-10-25 22:10:22 +0000

    Update azurehound.vm to v2.1.4

--- packages/azurehound.vm/tools/chocolateyinstall.ps1
+++ packages/azurehound.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'AzureHound'
 $category = 'Reconnaissance'
 
-$zipUrl = 'https://github.com/BloodHoundAD/AzureHound/releases/download/v2.1.3/azurehound-windows-amd64.zip'
-$zipSha256 = '5cc2668971e79f20065f0e9ebbff2962a35d99e081da9b8cfd75041ec26e6624'
+$zipUrl = 'https://github.com/BloodHoundAD/AzureHound/releases/download/v2.1.4/azurehound-windows-amd64.zip'
+$zipSha256 = '1607160ae684da22346cb9df960e8702d5d717d2bb64c2a3882234694b58de0c'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256
