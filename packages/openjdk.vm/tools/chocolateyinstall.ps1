d27570e16705396e2255beb1624f9bfb51577620
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-10-25 18:52:11 +0200

    Add openjdk.vm
    Metapackage for OpenJDK to ensure all packages use the same OpenJDK
    version. The package also ensure that .jar files are executed with
    javaw.exe by default.

---
+++ packages/openjdk.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  # Execute .jar files with javaw.exe by default
+  $javaw_path = "$Env:ProgramFiles\OpenJDK\jdk-21.0.1\bin\javaw.exe"
+  VM-Assert-Path $javaw_path
+  cmd /c assoc .jar=jarfile
+  cmd /c ftype jarfile=`"$javaw_path`" -jar `"%1`" %*
+} catch {
+    VM-Write-Log-Exception $_
+}
+
