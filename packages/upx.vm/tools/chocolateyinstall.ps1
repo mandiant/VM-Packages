e0c68ac40673fb5e708af51ed454ef2360f8c835
vm-packages <vm-packages@mandiant.com>
2023-11-01 23:51:04 +0000

    Update upx.vm to 4.2.1

--- packages/upx.vm/tools/chocolateyinstall.ps1
+++ packages/upx.vm/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'upx'
 $category = 'Packers'
 
-$zipUrl = "https://github.com/upx/upx/releases/download/v4.2.0/upx-4.2.0-win32.zip"
-$zipSha256 = "fb1a924136b35e56cd1dbd761dff53fcc9757ccd3210cdbc8ffbd2904dcb66f3"
-$zipUrl_64 = 'https://github.com/upx/upx/releases/download/v4.2.0/upx-4.2.0-win64.zip'
-$zipSha256_64 = '83833b414d17b39f334de28b981f1a6a88252294b9d76005f2a8047f5c1821f1'
+$zipUrl = "https://github.com/upx/upx/releases/download/v4.2.1/upx-4.2.1-win32.zip"
+$zipSha256 = "475504d9b2ae5fd9ede27919ee3b3fa8869a1398645c1239fc19193022054268"
+$zipUrl_64 = 'https://github.com/upx/upx/releases/download/v4.2.1/upx-4.2.1-win64.zip'
+$zipSha256_64 = 'b6e20e35303a390c3b1211f5ed0559def2c34ec5774176bb22afee19b35b2138'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -zipUrl_64 $zipUrl_64 -zipSha256_64 $zipSha256_64 -consoleApp $true -innerFolder $true
