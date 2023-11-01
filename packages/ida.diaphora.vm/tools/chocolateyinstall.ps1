cca290443c72b7873dccc4a8f602f50ed670d710
vm-packages <vm-packages@mandiant.com>
2023-11-01 22:16:16 +0000

    Update ida.diaphora.vm to 3.1.1

--- packages/ida.diaphora.vm/tools/chocolateyinstall.ps1
+++ packages/ida.diaphora.vm/tools/chocolateyinstall.ps1
@@ -5,8 +5,8 @@ $toolName = 'diaphora'
 $category = 'Utilities'
 $executableName = "diaphora.py"
 
-$zipUrl = 'https://github.com/joxeankoret/diaphora/archive/refs/tags/3.1.zip'
-$zipSha256 = '5802ebca119d2af4bb99434ce575dc4299396e95c7c6b01895d4a34c8d9d559d'
+$zipUrl = 'https://github.com/joxeankoret/diaphora/archive/refs/tags/3.1.1.zip'
+$zipSha256 = 'c7315edbcbd73ea4197779dec4f356e44dc9d4fc23065796fec5f02cb0088457'
 
 # Diaphora needs to be executed from IDA, do not install bin file
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -executableName $executableName -innerFolder $true -withoutBinFile
