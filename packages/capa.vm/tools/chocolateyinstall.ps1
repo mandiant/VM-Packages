9b8ef639145f8ddfb3362dca6d5dadc3d2219417
Naacbin <naacbin@protonmail.com>
2023-12-01 14:45:45 +0100

    Refactor help argument

--- packages/capa.vm/tools/chocolateyinstall.ps1
+++ packages/capa.vm/tools/chocolateyinstall.ps1
@@ -7,5 +7,4 @@ $category = 'Utilities'
 $zipUrl = "https://github.com/mandiant/capa/releases/download/v6.1.0/capa-v6.1.0-windows.zip"
 $zipSha256 = "070923d5ca225ef29a670af9cc66a8d648fcaaff7e283cb1ddc73de6e3610f0f"
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
-
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -arguments "--help"
