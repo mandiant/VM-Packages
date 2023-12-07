b93e9e423ca5458d4c9fa549604892e982f5fb45
Naacbin <naacbin@protonmail.com>
2023-12-07 18:47:26 +0100

    Change sqlitebrowser to install without metapackage

--- packages/sqlitebrowser.vm/tools/chocolateyuninstall.ps1
+++ packages/sqlitebrowser.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,8 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'DB Browser for SQLite'
+$subToolName = 'DB Browser for SQLCipher'
 $category = 'Utilities'
 
-VM-Remove-Tool-Shortcut $toolName $category
+VM-Uninstall $toolName $category
+VM-Remove-Tool-Shortcut $subToolName $category
