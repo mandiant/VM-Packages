2965a1685e52c5c3815ed81931190a2ea5ca60bf
Genwei Jiang <binjo.cn@gmail.com>
2023-12-11 16:17:42 +0800

    Add googlechrome to VM-Packages
    To address issue #469, added:
    1. download/install Chrome by checking if installer's signature is valid
    2. add associations for opening url/html file in Chrome
    3. disable Chrome auto-update related services and registries

---
+++ packages/googlechrome.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,55 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolsPath = Split-Path $MyInvocation.MyCommand.Definition
+. $toolsPath\sfta.ps1
+. $toolsPath\helpers.ps1
+
+$downloadDir = Get-PackageCacheLocation
+$installer          = 'googlechromeinstaller.msi'
+$packageArgs        = @{
+    packageName     = $env:ChocolateyPackageName
+    file            = Join-Path $downloadDir $installer
+    url             = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise.msi'
+    url64bit        = 'https://dl.google.com/tag/s/dl/chrome/install/googlechromestandaloneenterprise64.msi'
+    fileType        = 'MSI'
+    silentArgs      = "/quiet /norestart /l*v `"$($env:TEMP)\$($env:chocolateyPackageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+    validExitCodes  = @(0)
+}
+
+# Download the installer
+$packageArgs['file'] = Get-ChocolateyWebFile @packageArgs
+
+Start-Sleep 2
+
+$sigValid = (Get-AuthenticodeSignature -FilePath $packageArgs['file']).Status -eq 'Valid'
+
+try {
+    if ($sigValid) {
+        $toolName = 'Google Chrome'
+        $category = 'Utilities'
+
+        VM-Write-Log "INFO" "`tSignature valid ... installing $category"
+        Install-ChocolateyInstallPackage @packageArgs
+
+        VM-Write-Log "INFO" "`tSetup associations"
+        Set-PTA ChromeHTML http
+        Set-PTA ChromeHTML https
+        Set-FTA ChromeHTML .htm
+        Set-FTA ChromeHTML .html
+
+        VM-Write-Log "INFO" "`tDisable update settings"
+        Disable-Chrome-Updates
+
+        # Delete Desktop shortcut
+        $desktopShortcut = Join-Path ${Env:Public} "Desktop\$toolName.lnk"
+        if (Test-Path $desktopShortcut) {
+            Remove-Item $desktopShortcut -Force -ea 0
+        }
+    } else {
+        $file = $packageArgs['file']
+        VM-Write-Log "WARN" "`tSignature for downloaded $file is not valid"
+    }
+} catch {
+    VM-Write-Log-Exception $_
+}
---
+++ packages/googlechrome.vm/tools/helpers.ps1
@@ -0,0 +1,35 @@
+# https://github.com/actions/runner-images/blob/6130ddb3c3914dd466c71a4606cbf7e2697e0a02/images/windows/scripts/build/Install-Chrome.ps1#L11
+function Disable-Chrome-Updates {
+    # Prepare firewall rules
+    # Write-Host "Adding the firewall rule for Google update blocking..."
+    New-NetFirewallRule -DisplayName "BlockGoogleUpdate" -Direction Outbound -Action Block -Program "C:\Program Files (x86)\Google\Update\GoogleUpdate.exe" | Out-Null
+
+    $googleServices = @('gupdate', 'gupdatem') | Get-Service
+    Stop-Service $googleServices
+    $googleServices.WaitForStatus('Stopped', "00:01:00")
+    $googleServices | Set-Service -StartupType Disabled | Out-Null
+
+    $regGoogleUpdatePath = "HKLM:\SOFTWARE\Policies\Google\Update"
+    $regGoogleUpdateChrome = "HKLM:\SOFTWARE\Policies\Google\Chrome"
+    ($regGoogleUpdatePath, $regGoogleUpdateChrome) | ForEach-Object {
+        New-Item -Path $_ -Force | Out-Null
+    }
+
+    $regGoogleParameters = @(
+        @{ Name = "AutoUpdateCheckPeriodMinutes"; Value = 00000000},
+        @{ Name = "UpdateDefault"; Value = 00000000 },
+        @{ Name = "DisableAutoUpdateChecksCheckboxValue"; Value = 00000001 },
+        @{ Name = "Update{8A69D345-D564-463C-AFF1-A69D9E530F96}"; Value = 00000000 },
+        @{ Path = $regGoogleUpdateChrome; Name = "DefaultBrowserSettingEnabled"; Value = 00000000 }
+    )
+
+    $regGoogleParameters | ForEach-Object {
+        $Arguments = $_
+        if (-not ($Arguments.Path)) {
+            $Arguments.Add("Path", $regGoogleUpdatePath)
+        }
+        $Arguments.Add("Force", $true)
+        New-ItemProperty @Arguments | Out-Null
+    }
+
+}
