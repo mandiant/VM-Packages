a9aeee9830e7eef2cf6f5318c5ca12ded936669e
Naacbin <naacbin@protonmail.com>
2023-12-12 09:27:56 +0100

    Add jadx

---
+++ packages/jadx.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'jadx'
+$category = 'Forensic'
+
+$exeUrl = 'https://github.com/skylot/jadx/releases/download/v1.4.7/jadx-gui-1.4.7-no-jre-win.exe'
+$exeSha256 = '1254d7f54037ec3b929e3e3eb9f9830a436fddefa95ef295d01cf79c75f845b9'
+
+VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $false
