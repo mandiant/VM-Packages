603f540a272af5f00db7de59ffc43879974de4c3
naacbin <58483316+naacbin@users.noreply.github.com>
2023-12-12 14:04:24 +0100

    Update category
    Co-authored-by: Ana María Martínez Gómez <anamaria@martinezgomez.name>

--- packages/jadx.vm/tools/chocolateyinstall.ps1
+++ packages/jadx.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'jadx'
-$category = 'Forensic'
+$category = 'Java & Android'
 
 $exeUrl = 'https://github.com/skylot/jadx/releases/download/v1.4.7/jadx-gui-1.4.7-no-jre-win.exe'
 $exeSha256 = '1254d7f54037ec3b929e3e3eb9f9830a436fddefa95ef295d01cf79c75f845b9'
