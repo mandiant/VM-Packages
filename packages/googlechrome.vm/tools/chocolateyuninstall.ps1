735f5fb5a6df6dad5a8ede071ec0b571d6bdede5
binjo <binjo.cn@gmail.com>
2023-12-12 22:35:03 +0800

    delete uninstaller of googlechrome.vm

--- packages/googlechrome.vm/tools/chocolateyuninstall.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = 'Continue'
-Import-Module vm.common -Force -DisableNameChecking
-
-$toolName = 'Chrome'
-$category = 'Utilities'
-
-VM-Remove-Tool-Shortcut $toolName $category
