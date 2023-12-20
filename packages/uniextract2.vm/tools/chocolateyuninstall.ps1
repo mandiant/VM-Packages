6446972928c9efba6343bbc14a463a14b36ad4aa
Ana Maria Martinez Gomez <ana.martinez@mandiant.com>
2023-12-20 12:53:34 +0100

    uniextract: Change category to Packers
    Closes https://github.com/mandiant/VM-Packages/issues/757

--- packages/uniextract2.vm/tools/chocolateyuninstall.ps1
+++ packages/uniextract2.vm/tools/chocolateyuninstall.ps1
@@ -2,6 +2,6 @@ $ErrorActionPreference = 'Continue'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'UniExtract'
-$category = 'Utilities'
+$category = 'Packers'
 
 VM-Uninstall $toolName $category
