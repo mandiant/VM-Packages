e4b0cd7280246ebea0f99d5850c5d69d28e5c4a1
Elliot Chernofsky <35854880+emtuls@users.noreply.github.com>
2024-01-04 13:37:14 -0500

    Adjust Apktool package to use openjdk

--- packages/apktool.vm/tools/chocolateyinstall.ps1
+++ packages/apktool.vm/tools/chocolateyinstall.ps1
@@ -4,18 +4,26 @@ Import-Module vm.common -Force -DisableNameChecking
 try {
   $toolName = 'apktool'
   $category = 'Java & Android'
-  $shimPath = 'bin\apktool.exe'
+  $rawToolPath = Join-Path ${Env:RAW_TOOLS_DIR} "$toolName"
 
-  $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
-  $shortcut = Join-Path $shortcutDir "$toolName.lnk"
+  # Download apktool.bat
+  $wrapperPath = Join-Path $rawToolPath "$toolName.bat"
+  $wrapperSource = 'https://raw.githubusercontent.com/iBotPeaches/Apktool/0741664808724bda41744ad3981bac2eec672d1b/scripts/windows/apktool.bat'
+  $wrapperChecksum = "3e1c29f9d2c7b3a7c938573f4c2ae61172f6221dc9febfa85080f354357d6336"
+  Get-ChocolateyWebFile -PackageName '$toolName wrapper script' -FileFullPath $wrapperPath -Url $wrapperSource -Checksum $wrapperChecksum -ChecksumType "sha256"
+  VM-Assert-Path $wrapperPath
 
-  $executablePath = Join-Path ${Env:ChocolateyInstall} $shimPath -Resolve
-  $executableCmd  = Join-Path ${Env:WinDir} "system32\cmd.exe"
-  $executableDir  = Join-Path ${Env:UserProfile} "Desktop"
-  $executableArgs = "/K `"cd `"$executableDir`" && `"$executablePath`""
+  # Download apktool.jar
+  $toolPath = Join-Path $rawToolPath "$toolName.jar"
+  $toolSource = 'https://github.com/iBotPeaches/Apktool/releases/download/v2.9.1/apktool_2.9.1.jar'
+  $toolChecksum = "de7ce8aa109acb649e7f69cfe91030ffc20dbcc46edd8abbf6c2d1e36cfccd7b"
+  Get-ChocolateyWebFile -PackageName $toolName -FileFullPath $toolPath -Url $toolSource -Checksum $toolChecksum -ChecksumType "sha256"
+  VM-Assert-Path $toolPath
 
-  Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executableCmd -Arguments $executableArgs -WorkingDirectory $executableDir
-  VM-Assert-Path $shortcut
+  # Add apktool to Path
+  VM-Add-To-Path $rawToolPath
+
+  VM-Install-Shortcut -toolname $toolname -category $category -executablePath $wrapperPath -consoleApp $true
 } catch {
   VM-Write-Log-Exception $_
 }
