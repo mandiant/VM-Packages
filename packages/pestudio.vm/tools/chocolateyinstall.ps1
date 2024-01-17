f432036fcbd67aea10d6904b0272eb91639fc852
vm-packages <vm-packages@mandiant.com>
2024-01-17 23:49:26 +0000

    Update pestudio.vm to 9.57

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'pestudio'
 $category = 'PE'
 
-$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio-9.56.zip'
-$zipSha256 = '57f55e9e6c0db64dc28517efacc919e53dc6afef91ea1e6aa1fbd7be1ec35cbd'
+$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio-9.57.zip'
+$zipSha256 = 'df0664f07bc1561788abfad101ac371e37310bb20f50d5171fb3edf65e950eeb'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
