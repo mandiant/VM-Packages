8edd03f77ce5400c07d7946de8b8ddf2c442098e
vm-packages <vm-packages@mandiant.com>
2024-02-14 23:59:44 +0000

    Update sclauncher64.vm to v0.0.4

--- packages/sclauncher64.vm/tools/chocolateyinstall.ps1
+++ packages/sclauncher64.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'SCLauncher64'
 $category = 'Utilities'
 
-$exeUrl = 'https://github.com/jstrosch/sclauncher/releases/download/v0.0.3/sclauncher64.exe'
-$exeSha256 = '0887c4b93ec9595f69a5d38596553d91a29cac4a299d68f3030127778c614da0'
+$exeUrl = 'https://github.com/jstrosch/sclauncher/releases/download/v0.0.4/sclauncher64.exe'
+$exeSha256 = 'c05f654e52a61be7f1a7ae94b0b408796732c145426be0e3de825b241b6054c5'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
