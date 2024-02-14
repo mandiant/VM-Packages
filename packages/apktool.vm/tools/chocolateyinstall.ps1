7e4af0cec8d85dedddce15b52387dbc552c4fcea
vm-packages <vm-packages@mandiant.com>
2024-02-14 22:10:23 +0000

    Update apktool.vm to 2.9.3

--- packages/apktool.vm/tools/chocolateyinstall.ps1
+++ packages/apktool.vm/tools/chocolateyinstall.ps1
@@ -15,8 +15,8 @@ try {
 
   # Download apktool.jar
   $toolPath = Join-Path $rawToolPath "$toolName.jar"
-  $toolSource = 'https://github.com/iBotPeaches/Apktool/releases/download/v2.9.2/apktool_2.9.2.jar'
-  $toolChecksum = "831f0ffc97b6f20f511d6183cbf6785464d341aacb0fb7e6f22ef0c7b228911a"
+  $toolSource = 'https://github.com/iBotPeaches/Apktool/releases/download/v2.9.3/apktool_2.9.3.jar'
+  $toolChecksum = "7956eb04194300ce0d0a84ad18771eebc94b89fb8d1ddcce8ea4c056818646f4"
   Get-ChocolateyWebFile -PackageName $toolName -FileFullPath $toolPath -Url $toolSource -Checksum $toolChecksum -ChecksumType "sha256"
   VM-Assert-Path $toolPath
 
