10fceceaef1fd7b7884fb443c49b8101d28139e4
vm-packages <vm-packages@mandiant.com>
2024-02-28 22:10:09 +0000

    Update cutter.vm to v2.3.3

--- packages/cutter.vm/tools/chocolateyinstall.ps1
+++ packages/cutter.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Cutter'
 $category = 'Disassemblers'
 
-$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.3.2/Cutter-v2.3.2-Windows-x86_64.zip'
-$zipSha256 = 'e53a137809a610f939ce13744640f0da67369e87fdb1b2545be3e417c7781c8e'
+$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.3.3/Cutter-v2.3.3-Windows-x86_64.zip'
+$zipSha256 = '1a0e52d42b5aacbb58cac55c25e579d6d3591a49fe672650f8037b73d96f4ac5'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
