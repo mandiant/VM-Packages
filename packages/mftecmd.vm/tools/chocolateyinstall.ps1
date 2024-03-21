a9b24253494f9d691a9505d5a0e7c489a27860b7
Naacbin <naacbin@protonmail.com>
2024-03-21 23:02:46 +0100

    Package update

--- packages/mftecmd.vm/tools/chocolateyinstall.ps1
+++ packages/mftecmd.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'MFTECmd'
 $category = 'Forensic'
 
 $zipUrl = 'https://f001.backblazeb2.com/file/EricZimmermanTools/net6/MFTECmd.zip'
-$zipSha256 = 'ce4313e33cf424fd102959d7c687c768c5075bffc4a6536765d017e7d30d443b'
+$zipSha256 = '9beb6bb054df4806023937548bec212177cb8967f6f4d84b73a4e35fb13b8a50'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
--- packages/timeline_explorer.vm/tools/chocolateyinstall.ps1
+++ packages/timeline_explorer.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'TimelineExplorer'
 $category = 'Forensic'
 
 $zipUrl = 'https://f001.backblazeb2.com/file/EricZimmermanTools/net6/TimelineExplorer.zip'
-$zipSha256 = '0542e719418d91ee7fa0d62a4b7af6003c72e8bd0ecc572ecd6fc0ab4c3a83e0'
+$zipSha256 = '61aceca2e41f62d52ce67642c9881789b0be070f18526dbe2e60a163a4ceb2f6'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $false -innerFolder $true
