ab3d170b9e5076b4841d0f5fdf7953bbc6b6027b
vm-packages <vm-packages@google.com>
2024-03-27 23:50:47 +0000

    Update snaffler.vm to 1.0.146

--- packages/snaffler.vm/tools/chocolateyinstall.ps1
+++ packages/snaffler.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Snaffler'
 $category = 'Reconnaissance'
 
-$exeUrl = 'https://github.com/SnaffCon/Snaffler/releases/download/1.0.140/Snaffler.exe'
-$exeSha256 = '71bb8b15b1fbab1ebe7cd7898397d8a8a627af06dc510437f25887aa0aa0e4e1'
+$exeUrl = 'https://github.com/SnaffCon/Snaffler/releases/download/1.0.146/Snaffler.exe'
+$exeSha256 = 'f1fd9e9170a4654219055b134a8f7fda8e11a0a8493d5bd641775db51586de17'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
