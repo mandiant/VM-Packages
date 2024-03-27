45b4d9f4ba8e05978ef880de2b699ba16277f009
vm-packages <vm-packages@google.com>
2024-03-27 23:50:09 +0000

    Update sclauncher64.vm to v0.0.5

--- packages/sclauncher64.vm/tools/chocolateyinstall.ps1
+++ packages/sclauncher64.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'SCLauncher64'
 $category = 'Shellcode'
 
-$exeUrl = 'https://github.com/jstrosch/sclauncher/releases/download/v0.0.4/sclauncher64.exe'
-$exeSha256 = 'c05f654e52a61be7f1a7ae94b0b408796732c145426be0e3de825b241b6054c5'
+$exeUrl = 'https://github.com/jstrosch/sclauncher/releases/download/v0.0.5/sclauncher64.exe'
+$exeSha256 = 'e7ec06d0f0110a4892d2c026f57392f36b25433cfc114fbcba81041a44d970c8'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
