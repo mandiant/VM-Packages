3091c7f5e591412a0104560ddf87d1f7068611aa
vm-packages <vm-packages@google.com>
2024-05-07 15:29:17 +0000

    Update aleapp.vm to v3.2.2

--- packages/aleapp.vm/tools/chocolateyinstall.ps1
+++ packages/aleapp.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'aleappGUI'
 $category = 'Forensic'
 
-$exeUrl = 'https://github.com/abrignoni/ALEAPP/releases/download/v3.2.1/aleappGUI.exe'
-$exeSha256 = '39d7a90c74ec49dbbc18051ae70798972f0934c9c1f6f97cf8c0c6cd701c665a'
+$exeUrl = 'https://github.com/abrignoni/ALEAPP/releases/download/v3.2.2/aleappGUI.exe'
+$exeSha256 = 'abca427a28765369d6985d68f5503790e551a686cff4c848bc6b3671b8966277'
 
 VM-Install-Single-Exe $toolName -category $category -exeUrl $exeUrl -exeSha256 $exeSha256 -consoleApp $false
