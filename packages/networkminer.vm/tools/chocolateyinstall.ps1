c268333920fe99f51d963867338c7fd293e48fb1
erik4711 <erik.hjelmvik@gmail.com>
2024-05-27 12:40:47 +0200

    NetworkMiner 2.9 Released
    https://www.netresec.com/?page=Blog&month=2024-05&post=NetworkMiner-2-9-Released

--- packages/networkminer.vm/tools/chocolateyinstall.ps1
+++ packages/networkminer.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'NetworkMiner'
 $category = 'Networking'
 
 $zipUrl = 'https://www.netresec.com/?download=NetworkMiner'
-$zipSha256 = '34e8ba09d6bb47463c0e154b7a8eef26922b8dd61000e95d1880aa8c175507e1'
+$zipSha256 = 'c610f6ba647ddd9c718e87018ee40595a4d72a52a6b3b7ceb53caf4fa8de6f05'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
