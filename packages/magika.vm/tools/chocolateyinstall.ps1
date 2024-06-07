aae87173b7d92a92c6dc893bb8697b2c5a495d83
Ana Maria Martinez Gomez <anamaria@martinezgomez.name>
2024-06-07 09:18:04 +0200

    Add helpers to install tools with Pip
    Add helper functions in `vm.common.psm1` to simplify installation of
    Python tools installed with pip. Use the new helpers in magika.vm, also
    fixing the following issues:
    - Fix uninstallation which wrongly used `VM-Uninstall`, as it is only
      needed to remove the shortcut in the Tools category.
    - Remove unused `$iconLocation` in installation. This change does not
      have any visible effect, but simplifies the code.
    
    Note the version of magika has been **decreased** because it was incorrect.

--- packages/magika.vm/tools/chocolateyinstall.ps1
+++ packages/magika.vm/tools/chocolateyinstall.ps1
@@ -1,16 +1,7 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
-try {
-    $toolName = 'magika'
-    $category = 'File Information'
+$toolName = 'magika'
+$category = 'File Information'
 
-    VM-Pip-Install $toolName
-
-    $executablePath = "$(where.exe $toolName)"
-    $arguments = "--help"
-
-    VM-Install-Shortcut $toolName $category $executablePath -consoleApp $true -arguments $arguments -iconLocation $iconLocation
-} catch {
-    VM-Write-Log-Exception $_
-}
+VM-Install-With-Pip -toolName $toolName -category $category
--- packages/magika.vm/tools/chocolateyuninstall.ps1
+++ packages/magika.vm/tools/chocolateyuninstall.ps1
@@ -4,6 +4,4 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'magika'
 $category = 'File Information'
 
-Invoke-Expression "py -3.10 -m pip uninstall $toolName -y"
-
-VM-Uninstall $toolName $category
+VM-Uninstall-With-Pip -toolName $toolName -category $category
