fd0b8a9526ef638776830222babeb29038570a0c
vm-packages <vm-packages@google.com>
2024-06-18 11:23:30 +0000

    Update ida.plugin.hashdb.vm to 1.9.1

--- packages/ida.plugin.hashdb.vm/tools/chocolateyinstall.ps1
+++ packages/ida.plugin.hashdb.vm/tools/chocolateyinstall.ps1
@@ -2,12 +2,12 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-    # Install requirements: https://github.com/OALabs/hashdb-ida/blob/1.9.0/requirements.txt
+    # Install requirements: https://github.com/OALabs/hashdb-ida/blob/1.9.1/requirements.txt
     VM-Pip-Install requests
 
     $pluginName = 'hashdb.py'
-    $pluginUrl = 'https://github.com/OALabs/hashdb-ida/releases/download/1.9.0/hashdb.py'
-    $pluginSha256 = '45c55bd5c234e42b02435f2c93637dc33c13fc2f92fd060bcd755533eaa2807d'
+    $pluginUrl = 'https://github.com/OALabs/hashdb-ida/releases/download/1.9.1/hashdb.py'
+    $pluginSha256 = 'bd2022af77d27fb447adcad71bb9b33a255e59f646ca6961a64c78e26d2c4066'
 
     VM-Install-IDA-Plugin -pluginName $pluginName -pluginUrl $pluginUrl -pluginSha256 $pluginSha256
 } catch {
