f939aff9ccc9973ee7ffdda18b92374e44b1a12c
Ana Maria Martinez Gomez <anamaria@martinezgomez.name>
2024-07-10 18:14:39 +0200

    [pdbs.pdbresym.vm] Download .NET PDBs
    Download .NET PDBs to `C:\symbols` using PDBReSym to iterate through
    `C:\Windows\Microsoft.NET`.

--- packages/pdbs.pdbresym.vm/tools/chocolateyinstall.ps1
+++ packages/pdbs.pdbresym.vm/tools/chocolateyinstall.ps1
@@ -2,10 +2,14 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
-  # Iterate through C:\Windows\System32 downloading all PDBs concurrently
   $executablePath = Join-Path ${Env:RAW_TOOLS_DIR} PDBReSym\PDBReSym.exe -Resolve
+
   VM-Write-Log "INFO" "Iterating through C:\Windows\System32 downloading PDBs to C:\symbols"
   & $executablePath cachesyms
+
+  VM-Write-Log "INFO" "Iterating through  C:\Windows\Microsoft.NET downloading .NET PDBs to C:\symbols"
+  & $executablePath cachesyms --sysdir "C:\Windows\Microsoft.NET"
+
   # The downloaded symbols are store into C:\symbols
   VM-Assert-Path "C:\symbols"
 
