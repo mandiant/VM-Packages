56fd880159fa3f4ff82e88cc0ef024fea625ddaf
vm-packages <vm-packages@google.com>
2024-10-16 22:10:47 +0000

    Update capa.vm to 7.4.0

--- packages/capa.vm/tools/chocolateyinstall.ps1
+++ packages/capa.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'capa'
 $category = 'Utilities'
 
-$zipUrl = "https://github.com/mandiant/capa/releases/download/v7.3.0/capa-v7.3.0-windows.zip"
-$zipSha256 = "fc37549772c51fc48a0505bd38fff8ba5faaf9be1c9f1f04328641d46aee6163"
+$zipUrl = "https://github.com/mandiant/capa/releases/download/v7.4.0/capa-v7.4.0-windows.zip"
+$zipSha256 = "4ec2f7cd025751ee897e4818cda4ae572969848053b913d4d3e84ed7ac0af040"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -arguments "--help"
