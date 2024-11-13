fdeea9b13b92bfca65de8f6db46e972c6c577401
Elliot Chernofsky <echernofsky@google.com>
2024-11-12 21:25:44 -0500

    Fix internet_detector not installing

--- packages/internet_detector.vm/tools/chocolateyinstall.ps1
+++ packages/internet_detector.vm/tools/chocolateyinstall.ps1
@@ -10,12 +10,12 @@ $toolDir = Join-Path ${Env:RAW_TOOLS_DIR} $toolName
 New-Item -Path $toolDir -ItemType Directory -Force -ea 0
 VM-Assert-Path $toolDir
 
-# Install pyinstaller (needed to build the Python executable) and tool dependencies ('pywin32')
-$dependencies = "pyinstaller,pywin32"
+# Install pyinstaller 6.11.1 (needed to build the Python executable with a version capable of executing in admin cmd) and tool dependencies ('pywin32')
+$dependencies = "pyinstaller==6.11.1,pywin32"
 VM-Pip-Install $dependencies
 
-# This wrapper is needed because we can't run PyInstaller as admin, so this forces a usermode context.
-Start-Process -FilePath 'cmd.exe' -ArgumentList "/c pyinstaller --onefile -w --distpath $toolDir --workpath $packageToolDir --specpath $packageToolDir $packageToolDir\internet_detector.pyw" -Wait
+# This wrapper is needed because PyInstaller emits an error when running as admin and this mitigates the issue.
+Start-Process -FilePath 'cmd.exe' -WorkingDirectory $toolDir -ArgumentList "/c pyinstaller --onefile -w --log-level FATAL --distpath $toolDir --workpath $packageToolDir --specpath $packageToolDir $packageToolDir\internet_detector.pyw" -Wait
 
 # Move images to %VM_COMMON_DIR% directory
 $imagesPath = Join-Path $packageToolDir "images"
