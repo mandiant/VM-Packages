f0b8ca7e32aaf798f8cd6e4ad97a5da5f152b729
vm-packages <vm-packages@google.com>
2024-11-22 12:25:44 +0000

    Update goresym.vm to 3.0.1

--- packages/goresym.vm/tools/chocolateyinstall.ps1
+++ packages/goresym.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'GoReSym'
 $category = 'File Information'
 
-$zipUrl = 'https://github.com/mandiant/GoReSym/releases/download/v2.7.4/GoReSym-windows.zip'
-$zipSha256 = '075a9e7f6b5494e90b9880d1f8a91bd4c16841ff74bfc0c72253e3558aef8a38'
+$zipUrl = 'https://github.com/mandiant/GoReSym/releases/download/v3.0.1/GoReSym-windows.zip'
+$zipSha256 = 'f8fdf6b597222cb8f6fcb92961b3a5b537d799cc152b909dd95cb09d4efe830e'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -arguments "--help"
