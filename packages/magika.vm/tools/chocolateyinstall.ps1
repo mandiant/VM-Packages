a6e5ca3abab1b45c05fa0de4a26de9b22b1b6b21
Elliot Chernofsky <echernofsky@google.com>
2024-11-25 00:36:54 -0500

    pin magika version to fix pip issue

--- packages/magika.vm/tools/chocolateyinstall.ps1
+++ packages/magika.vm/tools/chocolateyinstall.ps1
@@ -3,5 +3,6 @@ Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'magika'
 $category = 'File Information'
+$version = "==0.5.0"
 
-VM-Install-With-Pip -toolName $toolName -category $category
+VM-Install-With-Pip -toolName $toolName -category $category -version $version
