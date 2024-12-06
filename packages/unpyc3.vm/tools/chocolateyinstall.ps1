33dfcf444ce63282cde7ee2cbbf9088f2353b80b
Ana Maria Martinez Gomez <anamg@google.com>
2024-12-06 20:01:06 +0100

    Add unpyc3.vm

---
+++ packages/unpyc3.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,9 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'unpyc3'
+$category = 'Python'
+
+VM-Pip-Install "https://github.com/greyblue9/unpyc37-3.10/archive/c1486ce3cf5b8fdfb5065e9c81a73a61481ed9ff.zip"
+
+VM-Install-Shortcut -toolName $toolName -category $category -executablePath "py" -consoleApp $true -arguments "-3.10 -m unpyc.unpyc3"
