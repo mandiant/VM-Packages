ad1932f5a254856800bbd9abff2afd36bc57ce2d
Spenser Reinhardt <spereinh@cisco.com>
2025-01-06 10:02:48 -0500

    create Binary Ninja package

---
+++ packages/binaryninja-free.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+try {
+  $toolName = 'BinaryNinja Free'
+  $category = 'Disassemblers'
+
+  $packageArgs = @{
+    packageName  = ${Env:ChocolateyPackageName}
+    fileType     = 'exe'
+    silentArgs   = '/S /ALLUSERS=1'
+    url          = 'https://cdn.binary.ninja/installers/binaryninja_free_win64.exe'
+    checksum     = '426aa8219415a64df90562274ae7e420471934c60f3a19c459e982467469cf55'
+    checksumType = 'sha256'
+  }
+
+  Install-ChocolateyPackage @packageArgs
+
+  $toolDir = Join-Path ${Env:ProgramFiles} "Vector35"  -Resolve
+  $toolDir = Join-Path $toolDir "BinaryNinja"  -Resolve
+  $executablePath = Join-Path $toolDir "binaryninja.exe" -Resolve
+
+  Install-BinFile -Name $toolname -Path $executablePath
+  $executableIcon = Join-Path $toolDir "icon.ico" -Resolve
+  VM-Install-Shortcut -toolName "binja" -category $category -executablePath $executablePath -IconLocation $executableIcon
+
+} catch {
+  VM-Write-Log-Exception $_
+}
