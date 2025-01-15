d3860dab691264d4228c4fddeb3c921f888277d8
vm-packages <vm-packages@google.com>
2025-01-15 22:31:26 +0000

    Update sqlrecon.vm to 3.9

--- packages/sqlrecon.vm/tools/chocolateyinstall.ps1
+++ packages/sqlrecon.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'SQLRecon'
 $category = 'Exploitation'
 
-$exeUrl = 'https://github.com/skahwah/SQLRecon/releases/download/v3.8/SQLRecon.exe'
-$exeSha256 = '979e62d0b229c4e988ea4b655cd7d26a992c3eb1457c9418b6ac42ad79f4d756'
+$exeUrl = 'https://github.com/skahwah/SQLRecon/releases/download/v3.9/SQLRecon.exe'
+$exeSha256 = 'ec7cc5641356b27e2e4654e043382708d3fda1ede7989849f40832631e800566'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256
