362a7761108fbbd011d6e57a57fe925014889a3d
Spenser Reinhardt <spereinh@cisco.com>
2025-01-16 12:31:49 -0500

    binja: move to VM-Install-With-Installer

--- packages/binaryninja.vm/tools/chocolateyinstall.ps1
+++ packages/binaryninja.vm/tools/chocolateyinstall.ps1
@@ -1,28 +1,35 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
+$toolName = "binaryninja"
+$category = "Disassemblers"
+$installFile = "binaryninja_free_win64.exe"
+
+function Get-CurrentHash {
+  param (
+    [string]$url      = "https://binary.ninja/js/hashes.json",
+    [string]$installFile = $installFile
+  )
+  $json = Invoke-WebRequest -Uri $url
+  $json = ConvertFrom-Json $json
+  return $json.Hashes.$installFile
+}
+
 try {
-  $toolName = 'binaryninja'
-  $category = 'Disassemblers'
+  $url = "https://cdn.binary.ninja/installers/$installFile"
+  $hash = Get-CurrentHash
+  $toolDir = Join-Path ${Env:ProgramFiles} "Vector35"
+  $toolDir = Join-Path $toolDir "BinaryNinja"
+  $executablePath = Join-Path $toolDir "binaryninja.exe"
 
-  $packageArgs = @{
-    packageName  = ${Env:ChocolateyPackageName}
-    fileType     = 'exe'
-    silentArgs   = '/S /ALLUSERS=1'
-    url          = 'https://cdn.binary.ninja/installers/binaryninja_free_win64.exe'
-    checksum     = '426aa8219415a64df90562274ae7e420471934c60f3a19c459e982467469cf55'
-    checksumType = 'sha256'
-  }
-
-  Install-ChocolateyPackage @packageArgs
-
-  $toolDir = Join-Path ${Env:ProgramFiles} "Vector35"  -Resolve
-  $toolDir = Join-Path $toolDir "BinaryNinja"  -Resolve
-  $executablePath = Join-Path $toolDir "binaryninja.exe" -Resolve
-
-  Install-BinFile -Name $toolname -Path $executablePath
-  $executableIcon = Join-Path $toolDir "icon.ico" -Resolve
-  VM-Install-Shortcut -toolName "binja" -category $category -executablePath $executablePath -IconLocation $executableIcon
+  VM-Install-With-Installer -toolName $toolName `
+    -category $category `
+    -fileType 'EXE' `
+    -silentArgs '/S /ALLUSERS=1' `
+    -executablePath $executablePath `
+    -url $url `
+    -sha256 $hash `
+    -consoleApp $false
 
 } catch {
   VM-Write-Log-Exception $_
