7ddd292db34bc2c5d274888bc27a201386946f4b
Sara Rincon <sararn@google.com>
2025-01-17 12:43:57 +0100

    Make version to VM-Install-with-Pip mandatory
    Update tools installed with pip to include the version in the install script, required by VM-Install-with-PIP.

--- packages/autoit-ripper.vm/tools/chocolateyinstall.ps1
+++ packages/autoit-ripper.vm/tools/chocolateyinstall.ps1
@@ -3,5 +3,6 @@ Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'autoit-ripper'
 $category = 'Packers'
+$version = '==1.1.2'
 
-VM-Install-With-Pip -toolName $toolName -category $category
+VM-Install-With-Pip -toolName $toolName -category $category -version $version
