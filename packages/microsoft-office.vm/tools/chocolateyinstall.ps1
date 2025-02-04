5d55bbe8987f061694f51060d2d02f72ae4609c3
Ana Maria Martinez Gomez <anamg@google.com>
2025-02-04 09:11:12 +0100

    [microsoft-office.vm] Add verification & shortcuts
    Ensure Office tools useful for malware analysis are installed and create
    shortcuts in the Tools directory to make accessing them easier.

--- packages/microsoft-office.vm/tools/chocolateyinstall.ps1
+++ packages/microsoft-office.vm/tools/chocolateyinstall.ps1
@@ -2,8 +2,28 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 try {
+    $tools = @(
+        @{name = 'Word'; executable = 'WINWORD.EXE'},
+        @{name = 'Excel'; executable = 'EXCEL.EXE'},
+        @{name = 'PowerPoint'; executable = 'POWERPNT.EXE'},
+        @{name = 'OneNote'; executable = 'ONENOTE.EXE'}
+    )
+    $category = 'Documents'
+    $shortcutDir = Join-Path ${Env:TOOL_LIST_DIR} $category
+
     # Install with choco instead as dependency to provide params such the product
     choco install microsoft-office-deployment --params="'/DisableUpdate:TRUE  /Product:ProPlus2024Retail'" --no-progress
+
+    # Find the directory where the tools are installed
+    $officeDirectory = Resolve-Path "C:\Program Files**\Microsoft Office\root\Office16" | Select-Object -first 1
+
+    # Ensure the tools are installed and create shortcuts
+    forEach ($tool in $tools) {
+        $executablePath = Join-Path $officeDirectory $($tool.executable) -Resolve
+        $shortcut = Join-Path $shortcutDir "$($tool.name).lnk"
+        Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $executablePath
+        VM-Assert-Path $shortcut
+    }
 } catch {
     VM-Write-Log-Exception $_
 }
