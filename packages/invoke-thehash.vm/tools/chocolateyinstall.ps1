06f73ca60fec8ab37a2546ca1d1fc6b678d85f62
pcroft06 <58776463+PrajeetGuha@users.noreply.github.com>
2025-02-11 00:35:17 +0530

    Update chocolateyinstall.ps1

--- packages/invoke-thehash.vm/tools/chocolateyinstall.ps1
+++ packages/invoke-thehash.vm/tools/chocolateyinstall.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
-#vars for powersploit
+#vars for invoke-thehash
 $toolName = 'Invoke-TheHash'
 $category = 'Exploitation'
 
-# install invoke-thehash, import module, and list available invoke-thehash modules
+# install invoke-thehash and import module
 $zipUrl = 'https://github.com/Kevin-Robertson/Invoke-TheHash/archive/refs/heads/master.zip'
 $zipSha256 = '3eb5db79e4c05fefcd85518b0b155ae75f6475dfe758e88901e9bff2fed2db6f'
 $powershellCommand = 'Import-Module .\Invoke-TheHash.psd1;'
