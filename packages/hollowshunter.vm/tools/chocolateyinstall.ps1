6ca5b68c0937bd47969145fd0b3ca773a988401c
vm-packages <vm-packages@google.com>
2025-02-12 22:12:36 +0000

    Update hollowshunter.vm to 0.4.1

--- packages/hollowshunter.vm/tools/chocolateyinstall.ps1
+++ packages/hollowshunter.vm/tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'hollows_hunter'
 $category = 'Memory'
 
-$zipUrl = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.4.0.2/hollows_hunter32.zip'
-$zipSha256 = '81f9e36d9aac860a8cb8e49715e9c70af6c9a52daa619cac86d71f473c959838'
-$zipUrl_64 = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.4.0.2/hollows_hunter64.zip'
-$zipSha256_64 = '1616812ac67ac3113d4fb065ec51f4bb50b8461ce41a969708c7d9ae17dfb49d'
+$zipUrl = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.4.1/hollows_hunter32.zip'
+$zipSha256 = '4312354f4b5c2665131c1db7cc4572b6c4ff09a0b9a8aec911e57a0dab400972'
+$zipUrl_64 = 'https://github.com/hasherezade/hollows_hunter/releases/download/v0.4.1/hollows_hunter64.zip'
+$zipSha256_64 = '59610f266b1b6cbe12c6bb7103c2c9f388b9b70ff5fa9b51d0d084de38dd963f'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -zipUrl_64 $zipUrl_64 -zipSha256_64 $zipSha256_64 -consoleApp $true
 
