a0d7b4745161ab62991ebcc123511df7d8463c46
pcroft06 <58776463+PrajeetGuha@users.noreply.github.com>
2025-02-18 22:22:16 +0530

    Update chocolateyinstall.ps1 checksum and list module commands

--- packages/invoke-thehash.vm/tools/chocolateyinstall.ps1
+++ packages/invoke-thehash.vm/tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ $category = 'Lateral Movement'
 
 # install invoke-thehash and import module
 $zipUrl = 'https://github.com/Kevin-Robertson/Invoke-TheHash/archive/01ee90f934313acc7d09560902443c18694ed0eb.zip'
-$zipSha256 = '3eb5db79e4c05fefcd85518b0b155ae75f6475dfe758e88901e9bff2fed2db6f'
-$powershellCommand = 'Import-Module .\Invoke-TheHash.psd1;'
+$zipSha256 = '9bd8f52de5d1cc2ca39b79fb169699cda88e6c6826ba1e3f2c1890e4b970d41f'
+$powershellCommand = 'Import-Module .\Invoke-TheHash.psd1;Get-Command -Module Invoke-TheHash'
 VM-Install-From-Zip $toolName $category $zipUrl $zipSha256 -innerFolder $true -powershellCommand $powershellCommand
