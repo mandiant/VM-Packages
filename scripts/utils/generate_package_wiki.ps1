bcc1af6246d78ae4eb2b75854e6d9af80969b12b
Sara Rincon <sararn@google.com>
2025-02-20 12:25:43 +0100

    Update wiki that lists packages by category
    Update the script that generates the wiki with the list of packages
    by category. The new nuspec packages now contain the category even if
    it is not used to prevent packages from being uncategorized.

--- scripts/utils/generate_package_wiki.ps1
+++ scripts/utils/generate_package_wiki.ps1
@@ -4,44 +4,28 @@ $wikiContent = ""
 $packagesByCategory = @{}
 
 foreach ($package in $packages) {
-    $categoryFile = "$packagesDir/$package/tools/chocolateyinstall.ps1"
-    if (Test-Path $categoryFile) {
-        $categoryMatch = Select-String -Path $categoryFile -Pattern '\$category\s*=\s*["''](.+?)["'']' -AllMatches
-        if ($categoryMatch) {
-            $category = $categoryMatch.Matches.Groups[1].Value.Trim("'""")
-        } else {
-            $category = "Not Categorized"
-        }
-    } else {
-        $category = "Not Categorized"
-    }
 
-    if (-not ($packagesByCategory.ContainsKey($category))) {
-        $packagesByCategory[$category] = ""
+     try {
+         $nuspecFile = "$packagesDir\$package\$package.nuspec"
+         if (Test-Path $nuspecFile) {
+             $xml = [xml](Get-Content $nuspecFile)
+             $description = $xml.package.metadata.description
+             $category = $xml.package.metadata.tags
+             if (-not $category){
+                $category = "Not Categorized"
+             }
+             if (-not ($packagesByCategory.ContainsKey($category))) {
+             	$packagesByCategory[$category] = ""
+             }
+             $packagesByCategory[$category] += "| $package | $description |`n"
+           }
+        } catch {
+            Write-Warning "Error parsing nuspec $($package): $_"
     }
-
-    try {
-        $nuspecFile = "$packagesDir\$package\$package.nuspec"
-        if (Test-Path $nuspecFile) {
-            $xml = [xml](Get-Content $nuspecFile)
-            $description = $xml.package.metadata.description
-        } else {
-            $description = "Nuspec file not found."
-        }
-        if (-not $description) {
-            $description = "Description not found in .nuspec."
-        }
-    }
-    catch {
-        Write-Warning "Error getting description for $($package): $_"
-        $description = "Error retrieving description."
-    }
-
-    $packagesByCategory[$category] += "| $package | $description |`n"
 }
 
 # Process categories (excluding "Not Categorized")
-$sortedCategories = ($packagesByCategory.Keys | Where-Object {$_ -ne "Not Categorized"} | Sort-Object)
+$sortedCategories = ($packagesByCategory.Keys | Where-Object {$_ -ne "Not Categorized"}  |Sort-Object)
 foreach ($category in $sortedCategories) {
     $wikiContent += "## $category`n`n"
     $wikiContent += "| Package | Description |`n"
@@ -49,13 +33,7 @@ foreach ($category in $sortedCategories) {
     $wikiContent += $packagesByCategory[$category] + "`n`n"
 }
 
-# Add "Not Categorized" last
-if ($packagesByCategory.ContainsKey("Not Categorized")) {
-    $wikiContent += "## Not Categorized`n`n"
-    $wikiContent += "| Package | Description |`n"
-    $wikiContent += "|---|---|`n"
-    $wikiContent += $packagesByCategory["Not Categorized"] + "`n`n"
-}
+
 
 # Write the wiki content to a file
 $wikiContent | Out-File -FilePath "wiki/Packages.md" -Encoding UTF8
