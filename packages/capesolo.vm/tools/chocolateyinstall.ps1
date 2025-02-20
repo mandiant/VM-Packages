f6d2a612a4eea67508e1f758a90fd90bd4689065
Sara Rincon <sararn@google.com>
2025-02-20 10:27:53 +0100

    Update category in the capesolo package
    Update the nuspec package and the install script to retrieve the category
    from the nuspec

--- packages/capesolo.vm/tools/chocolateyinstall.ps1
+++ packages/capesolo.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $toolName = 'CAPEsolo'
-$category = 'Debuggers'
+$category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 $version = '==0.4.23'
 
 VM-Install-With-Pip -toolName $toolName -category $category -version $version -arguments ""
