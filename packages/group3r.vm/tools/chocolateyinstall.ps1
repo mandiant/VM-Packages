dabc0199e8aada4a45a5096513be37adfce7390c
vm-packages <vm-packages@google.com>
2025-03-05 22:14:51 +0000

    Update group3r.vm to 1.0.67

--- packages/group3r.vm/tools/chocolateyinstall.ps1
+++ packages/group3r.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Group3r'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$exeUrl = 'https://github.com/Group3r/Group3r/releases/download/1.0.65/Group3r.exe'
-$exeSha256 = '10accf5038dd9a3353d50e63d208c684ddfe8df4d06b33602fada0f44a739039'
+$exeUrl = 'https://github.com/Group3r/Group3r/releases/download/1.0.67/Group3r.exe'
+$exeSha256 = '4c3fa4168e5b406f8b4ad0d61e38b85388b04fcaed2abab4c5dc51462d1d515b'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
