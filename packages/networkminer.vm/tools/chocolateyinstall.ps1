9cb18b33df3f955a3a93e8d386dcaf893e86721d
erik4711 <erik.hjelmvik@gmail.com>
2025-04-04 20:21:41 +0200

    NetworkMiner 3.0 Released (#1330)
    [networkminer.vm] Update to version 3.0
    
    Update NetworkMiner to the just released version 3.0.

--- packages/networkminer.vm/tools/chocolateyinstall.ps1
+++ packages/networkminer.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'NetworkMiner'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://download.netresec.com/networkminer/NetworkMiner_2-9.zip'
-$zipSha256 = 'c610f6ba647ddd9c718e87018ee40595a4d72a52a6b3b7ceb53caf4fa8de6f05'
+$zipUrl = 'https://download.netresec.com/networkminer/NetworkMiner_3-0.zip'
+$zipSha256 = '5d074a54e2f2f26d0a2cf5a2833ab08345f1a0eeba2bdf746835545ec23e3032'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
