3752309da8d680bc4dda6b48527dc261b884f2b1
PrajeetGuha <prajeetguha@gmail.com>
2025-04-10 21:15:35 +0530

    fix pestudio package

--- packages/pestudio.vm/tools/chocolateyinstall.ps1
+++ packages/pestudio.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'pestudio'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio-9.60.zip'
-$zipSha256 = 'acc8eac2b5060d81c1285c2c6b050d18a045b7c659e6bf11cfcaada3c00bd1dd'
+$zipUrl = 'https://www.winitor.com/tools/pestudio/current/pestudio.zip'
+$zipSha256 = 'c1e2d0c1fbf5951486cf3d850cc24b11b66e25e0a5b77a623e2eb13ffad9ddd9'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
