54d4454d7751c838c5d18af8276d024bd35202e2
Sara Rincon <sararn@google.com>
2025-04-13 14:16:24 +0200

    Add new node package electron/asar
    Add flag --logleve=true to npm install to ignore warnings
    Add flag --no-update-notifier to prevent the error `npm notice`
    Rename package name electron-asar to asar.vm

---
+++ packages/asar.vm/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+$ErrorActionPreference = 'Stop'
+Import-Module vm.common -Force -DisableNameChecking
+
+$toolName = 'asar'
+$category = VM-Get-Category($MyInvocation.MyCommand.Definition)
+$arguments = '--help'
+
+try {
+    # "--no-update-notifier" removes the npm funding notice that may cause an error
+    npm install -g '@electron/asar' --loglevel=error --no-update-notifier
+    VM-Install-Shortcut -toolName $toolName -category $category -arguments "$toolName $arguments" -powershell
+} catch {
+    VM-Write-Log-Exception $_
+}
