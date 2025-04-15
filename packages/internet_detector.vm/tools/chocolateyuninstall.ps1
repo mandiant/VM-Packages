fe57a7f12579a9eaf0e3e91268b821de1547fc71
d35ha <d35hax@gmail.com>
2025-04-15 18:45:39 +0800

    Filtering the internet_detector tool ICMP requests at fakenet

--- packages/internet_detector.vm/tools/chocolateyuninstall.ps1
+++ packages/internet_detector.vm/tools/chocolateyuninstall.ps1
@@ -6,3 +6,8 @@ $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 VM-Uninstall $toolName $category
 Unregister-ScheduledTask -TaskName 'Internet Detector' -Confirm:$false
+
+$fakenetConfig = "$Env:RAW_TOOLS_DIR\fakenet\fakenet3.5\configs\default.ini"
+$config = Get-Content -Path $fakenetConfig
+$config = $config -replace '^.*BlackListIDsICMP.*$', "# BlackListIDsICMP: 1234"
+Set-Content -Path $fakenetConfig -Value $config -Encoding UTF8 -Force
