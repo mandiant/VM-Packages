90e5881ecb265b596dab5e92a4c83b5974347c85
d35ha <d35hax@gmail.com>
2025-04-16 18:45:04 +0800

    Start the internet detector at user logon immediately

--- packages/internet_detector.vm/tools/chocolateyuninstall.ps1
+++ packages/internet_detector.vm/tools/chocolateyuninstall.ps1
@@ -5,4 +5,8 @@ $toolName = 'internet_detector'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 VM-Uninstall $toolName $category
-Unregister-ScheduledTask -TaskName 'Internet Detector' -Confirm:$false
+Disable-ScheduledTask -TaskName 'Internet Detector' -ea 0 | ForEach-Object {
+    $_ | Stop-ScheduledTask
+    $_ | Unregister-ScheduledTask -Confirm:$false
+}
+Stop-Process -Name "$toolName.exe" -Force -ea 0
