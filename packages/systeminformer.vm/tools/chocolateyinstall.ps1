12f1ba123e1669df712b3bdaff002615be1a3666
vm-packages <vm-packages@google.com>
2025-04-24 09:05:52 +0000

    Update systeminformer.vm to 3.2.25113.1114

--- packages/systeminformer.vm/tools/chocolateyinstall.ps1
+++ packages/systeminformer.vm/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'SystemInformer'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/winsiderss/si-builds/releases/download/3.2.25091.638/systeminformer-3.2.25091.638-release-bin.zip'
-$zipSha256 = 'c5204b0ac5cc47e172f6998dedfe2f5f7d9f522f1545b2e0beca79f00aaed319'
+$zipUrl = 'https://github.com/winsiderss/si-builds/releases/download/3.2.25113.1114/systeminformer-3.2.25113.1114-release-bin.zip'
+$zipSha256 = '43a19bd901f68e1e1057cffd0b8b0d30fb596acb03b3e1409ff055fe4b6bbdc8'
 $executableName = "amd64\$toolName.exe"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -executableName $executableName -consoleApp $false
