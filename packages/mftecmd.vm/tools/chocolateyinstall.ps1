87be76b9feab843fe045ba0683d6c9e721b98657
vm-packages <vm-packages@google.com>
2025-04-24 08:59:20 +0000

    Fix broken hash in mftecmd.vm

--- packages/mftecmd.vm/tools/chocolateyinstall.ps1
+++ packages/mftecmd.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'MFTECmd'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/net6/MFTECmd.zip'
-$zipSha256 = 'c7f20ce740f49a738f7228493f0764a09d23fb1dd67502b7cdd32d73fc3383f3'
+$zipSha256 = 'c73177af81e26440f819bf39ce925c5a2b6af3f1830d92da4dcfaddb173bb405'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
