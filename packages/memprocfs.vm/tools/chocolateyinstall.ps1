f8f73819b9bb76d95718f0bc59198746faa33f6c
Ana Maria Martinez Gomez <anamg@google.com>
2025-05-07 15:32:00 +0200

    [memprocfs.vm] Update to fix package
    The previously used release seems to have been deleted. Update it to fix
    the package.
    
    This also fixes a version mismatch: The bot wrongly updated the version
    of memprocfs.vm from 5.14.9 to 5.14 in
    https://github.com/mandiant/VM-Packages/pull/1361 because of a bug in
    update_package.py.

--- packages/memprocfs.vm/tools/chocolateyinstall.ps1
+++ packages/memprocfs.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'MemProcFS'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.14/MemProcFS_files_and_binaries_v5.14.9-win_x64-20250411.zip'
-$zipSha256 = 'ef13fb54fb4a44a1ac758684d3fe473ad91f5c3195f8862de2844860b4bd627d'
+$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.14/MemProcFS_files_and_binaries_v5.14.11-win_x64-20250501.zip'
+$zipSha256 = '40ce4ac604933dd919444a9cee42431b3fe43813fa4155bef17466b7db03d764'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
