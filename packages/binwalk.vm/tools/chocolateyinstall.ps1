ec979157365f0410ab6d4656a0716dc85a99df16
socketz <socketz@users.noreply.github.com>
2025-05-29 00:01:50 +0200

    Added workaround using VM-Install-From-Zip for binwalk v3.1.1

--- packages/binwalk.vm/tools/chocolateyinstall.ps1
+++ packages/binwalk.vm/tools/chocolateyinstall.ps1
@@ -4,8 +4,12 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'binwalk'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/ReFirmLabs/binwalk/archive/refs/tags/v3.1.0.zip'
-$zipSha256 = 'fabf028b31679ffea7a4b10df5ab7be7d73eb1ff3f7772909fb7955c85003efe'
+# Temporary workaround until official repo publishes a release automatically, replace with official URL when available
+# Note: The official binwalk repository does not currently provide a Windows binary.
+# This script uses a pre-built binary from a third-party source.
+# Replace with https://github.com/ReFirmLabs/binwalk when they provide a Windows binary.
+$zipUrl = 'https://github.com/socketz/binwalk/releases/download/v3.1.1/binwalk-Windows-msvc-x86_64.zip'
+$zipSha256 = '5418e2d88af47d89b7f5fe47449d47acdf1ded1aabdccc582aabbbca6d39da89'
 $arguments = ''
 
-VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $false -innerFolder $false -arguments $arguments
+VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false -arguments $arguments
