fc8c147cf93ec449f20fb36611cfd9244f5389b0
ninyya <Andreas@korsbakke.se>
2025-06-05 02:07:26 +0200

    Update chocolateyinstall.ps1

--- packages/memprocfs.vm/tools/chocolateyinstall.ps1
+++ packages/memprocfs.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'MemProcFS'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.14/MemProcFS_files_and_binaries_v5.14.13-win_x64-20250530.zip'
-$zipSha256 = '40ce4ac604933dd919444a9cee42431b3fe43813fa4155bef17466b7db03d764'
+$zipSha256 = '78A0BE8BE783EE4886926D8B7A1DACD19FB5EAA8E0FDC4822D3541CC7775105C'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
