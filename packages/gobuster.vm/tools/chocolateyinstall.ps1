c15b9c6cf709a2beaab14279babb34fd1bd37937
vm-packages <vm-packages@google.com>
2025-07-02 22:13:26 +0000

    Update gobuster.vm to 3.7.0

--- packages/gobuster.vm/tools/chocolateyinstall.ps1
+++ packages/gobuster.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'GoBuster'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = "https://github.com/OJ/gobuster/releases/download/v3.6.0/gobuster_Windows_x86_64.zip"
-$zipSha256 = "23403da32e153849000d5e930506a46d0950bad2b0dcb5a7d325793f0ec8cf19"
+$zipUrl = "https://github.com/OJ/gobuster/releases/download/v3.7.0/gobuster_Windows_x86_64.zip"
+$zipSha256 = "424595b70c2084c8f7570f292c23cdb82ec1eae7b64baa5d50d92d5fd6885c51"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true
