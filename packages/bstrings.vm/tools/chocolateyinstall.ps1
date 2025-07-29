6c2e90b7d98f41ec7677674b547edf247a7f14d5
vm-packages <vm-packages@google.com>
2025-07-29 17:27:52 +0000

    Update wireshark.vm to 4.4.8

--- packages/bstrings.vm/tools/chocolateyinstall.ps1
+++ packages/bstrings.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'bstrings'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/net6/bstrings.zip'
-$zipSha256 = '6fb35d4e0b57726e5ff8c1641803306049bcfeed1adb0b93d5f59b88b4146a18'
+$zipSha256 = 'BF1E806C9D94EE53905DA0FB247A15E4A27D29B26CE8A3EDBCA17081A902CE77'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
