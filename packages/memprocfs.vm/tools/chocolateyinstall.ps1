73b4406d9d898ee62f3c72ce36837b91ce85f398
Mustafa Nasser <d35hax@gmail.com>
2025-07-30 17:11:55 +0800

    Update the MemProcFS link
    This commit updates the link and the version of MemProcFS as the old
    link doesn't work anymore.

--- packages/memprocfs.vm/tools/chocolateyinstall.ps1
+++ packages/memprocfs.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'MemProcFS'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.14/MemProcFS_files_and_binaries_v5.14.11-win_x64-20250501.zip'
-$zipSha256 = '40ce4ac604933dd919444a9cee42431b3fe43813fa4155bef17466b7db03d764'
+$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.15/MemProcFS_files_and_binaries_v5.15.2-win_x64-20250711.zip'
+$zipSha256 = '1442a2bcbd206650c2bf818fd9956e0937c44231d11de5b6990a9de678d168bd'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
