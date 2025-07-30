8e9dd1aa0c3e8cf9dd622fcff2b1cbfa7fcda476
Mustafa Nasser <d35hax@gmail.com>
2025-07-30 19:23:55 +0800

    Fix the SHA256 hash mismatches

--- packages/bstrings.vm/tools/chocolateyinstall.ps1
+++ packages/bstrings.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'bstrings'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/net6/bstrings.zip'
-$zipSha256 = '8de97628d13a76a27873cb9a796b14c58119079b0ddbe599bb0c4f4ee06af691'
+$zipSha256 = '6fb35d4e0b57726e5ff8c1641803306049bcfeed1adb0b93d5f59b88b4146a18'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
