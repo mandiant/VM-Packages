67a5efaa4d742bdf44a9a127f14b667cd17dbd88
vm-packages <vm-packages@google.com>
2025-09-17 04:20:37 +0000

    Update ida.plugin.sigmaker.vm to 1.0.10

--- packages/ida.plugin.sigmaker.vm/tools/chocolateyinstall.ps1
+++ packages/ida.plugin.sigmaker.vm/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 Import-Module vm.common -Force -DisableNameChecking
 
 $pluginName = 'SigMaker64.dll'
-$pluginUrl = 'https://github.com/A200K/IDA-Pro-SigMaker/releases/download/v1.0.9/SigMaker9_64.dll'
-$pluginSha256 = 'cec05a96867f51dff686d72384f6be98c763dfda9da2ebbe653510620bfb7070'
+$pluginUrl = 'https://github.com/A200K/IDA-Pro-SigMaker/releases/download/v1.0.10/SigMaker9_64.dll'
+$pluginSha256 = 'd7c48abcb384ea5bdfdb909714e98aa3b5ac55021b129ef146cd80b5df81fff1'
 
 VM-Install-IDA-Plugin -pluginName $pluginName -pluginUrl $pluginUrl -pluginSha256 $pluginSha256
