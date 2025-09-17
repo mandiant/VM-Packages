5d2b64b552746835d8437031d3098dc5082eaf5e
vm-packages <vm-packages@google.com>
2025-09-17 04:14:02 +0000

    Fix broken hash in bstrings.vm

--- packages/bstrings.vm/tools/chocolateyinstall.ps1
+++ packages/bstrings.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'bstrings'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://download.mikestammer.com/net6/bstrings.zip'
-$zipSha256 = 'BF1E806C9D94EE53905DA0FB247A15E4A27D29B26CE8A3EDBCA17081A902CE77'
+$zipSha256 = 'bf1e806c9d94ee53905da0fb247a15e4a27d29b26ce8a3edbca17081a902ce77'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
