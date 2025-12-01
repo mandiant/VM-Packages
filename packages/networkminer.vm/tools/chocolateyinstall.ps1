a6e65f2d68a04398834251a578c4432fe5a198f2
erik4711 <erik.hjelmvik@gmail.com>
2025-12-01 11:19:00 +0100

    Update chocolateyinstall.ps1
    NetworkMiner 3.1 released
    https://netresec.com/?b=25C4039

--- packages/networkminer.vm/tools/chocolateyinstall.ps1
+++ packages/networkminer.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'NetworkMiner'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://download.netresec.com/networkminer/NetworkMiner_3-0.zip'
-$zipSha256 = '5d074a54e2f2f26d0a2cf5a2833ab08345f1a0eeba2bdf746835545ec23e3032'
+$zipUrl = 'https://download.netresec.com/networkminer/NetworkMiner_3-1.zip'
+$zipSha256 = '782e3d4d0b917a5aadf59966ab7d21ab30dbd593eff14e426f2b580a2e3f89e1'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
