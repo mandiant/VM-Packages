56157fcca244cb3af57ed03a2f9dfe30fcd160ff
Elliot Chernofsky <echernofsky@google.com>
2025-12-15 20:21:33 +0000

    Update openJDK to 25.0.0

--- packages/openjdk.vm/tools/chocolateyinstall.ps1
+++ packages/openjdk.vm/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ Import-Module vm.common -Force -DisableNameChecking
 
 try {
   # Execute .jar files with javaw.exe by default
-  $javaw_path = "$Env:ProgramFiles\OpenJDK\jdk-21.0.1\bin\javaw.exe"
+  $javaw_path = "$Env:ProgramFiles\OpenJDK\jdk-25\bin\javaw.exe"
   VM-Assert-Path $javaw_path
   cmd /c assoc .jar=jarfile
   cmd /c ftype jarfile=`"$javaw_path`" -jar `"%1`" %*
