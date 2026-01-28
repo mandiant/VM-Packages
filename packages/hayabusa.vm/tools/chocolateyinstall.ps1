addaead037da7dc44fc3727b7dae32bc03d41ecf
vm-packages <vm-packages@google.com>
2026-01-28 22:21:12 +0000

    Update hayabusa.vm to 3.7.0

--- packages/hayabusa.vm/tools/chocolateyinstall.ps1
+++ packages/hayabusa.vm/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'hayabusa'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = "https://github.com/Yamato-Security/hayabusa/releases/download/v3.6.0/hayabusa-3.6.0-win-x64.zip"
-$zipSha256 = 'e5efc3765277c686d00aca27b2454207e3a9f12231deec5697397519d70675b7'
+$zipUrl = "https://github.com/Yamato-Security/hayabusa/releases/download/v3.7.0/hayabusa-3.7.0-win-x64.zip"
+$zipSha256 = '6c5ed99560d0896bb8a38d53f19da5958c461a519d78b989dfdc8462fbf8b227'
 
-$executableName = $toolName.ToLower() + "-3.6.0-win-x64.exe"
+$executableName = $toolName.ToLower() + "-3.7.0-win-x64.exe"
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false -executableName $executableName
