a8c70c17cb2f61ac586e59fed233ab10a7738469
vm-packages <vm-packages@google.com>
2026-01-28 22:20:40 +0000

    Update gowitness.vm to 3.1.1

--- packages/gowitness.vm/tools/chocolateyinstall.ps1
+++ packages/gowitness.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'GoWitness'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$exeUrl = 'https://github.com/sensepost/gowitness/releases/download/3.0.5/gowitness-3.0.5-windows-amd64.exe'
-$exeSha256 = '45284210727f2d22774e07313db974a0fe9e9108687a7c091218e7e98b041bea'
+$exeUrl = 'https://github.com/sensepost/gowitness/releases/download/3.1.1/gowitness-3.1.1-windows-amd64.exe'
+$exeSha256 = '26ea2da2d7d4ef04e60289c94ecfd43692d6eca3723cfe318a03bda7a43f9374'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true
