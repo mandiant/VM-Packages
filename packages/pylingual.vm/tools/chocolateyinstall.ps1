d3354da6971b272b3a8ffd02b5205e6873b8c001
Elliot Chernofsky <echernofsky@google.com>
2026-03-20 08:22:23 +0000

    update pylingual

--- packages/pylingual.vm/tools/chocolateyinstall.ps1
+++ packages/pylingual.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'pylingual'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-VM-Pip-Install 'https://github.com/syssec-utd/pylingual/archive/9f0ba2aa0195ce853b65e1064e8aaae09b67966f.zip'
+VM-Pip-Install 'https://github.com/syssec-utd/pylingual/archive/99c74eeff5262c0200a3d378298af1f736e20b01.zip'
 
 $pyPath = (Get-Command py).Source
 VM-Install-Shortcut $toolName $category -executablePath $toolName -consoleApp $true -arguments "--help" -iconLocation $pyPath
