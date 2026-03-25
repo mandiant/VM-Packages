f7c25747dbcc3b563ade69e8645cdb9dd9cf4fe9
vm-packages <vm-packages@google.com>
2026-03-25 22:40:58 +0000

    Update seclists.vm to 2026.1

--- packages/seclists.vm/tools/chocolateyinstall.ps1
+++ packages/seclists.vm/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'SecLists'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/danielmiessler/SecLists/archive/refs/tags/2025.3.zip'
-$zipSha256 = 'ed10bd42a3c1ecc68a8bca52b3cea3ff40aa8be39e0d0522b1a399223a1b2c47'
+$zipUrl = 'https://github.com/danielmiessler/SecLists/archive/refs/tags/2026.1.zip'
+$zipSha256 = 'fe72b89126070f5b1b32acfa408699e4d1d6a9030380f5ff257735af358c4b31'
 
 # This tool does not have a `.exe` associated with it, so this links it to the directory
 VM-Install-From-Zip $toolName $category $zipUrl $zipSha256 -withoutBinFile -innerFolder $true
