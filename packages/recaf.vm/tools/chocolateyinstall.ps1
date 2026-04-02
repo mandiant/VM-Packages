e7a9ae03352104752502f09ac0072182144233a0
Elliot Chernofsky <echernofsky@google.com>
2026-04-02 03:09:16 -0400

    fix recaf download link

--- packages/recaf.vm/tools/chocolateyinstall.ps1
+++ packages/recaf.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'recaf-4.0.0'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$exeUrl = 'https://github.com/Col-E/Recaf/releases/download/4.0.0-alpha/recaf-4x-alpha-win-86x64.jar'
-$exeSha256 = '5a4b960521b4008f44f5c887376da55d4c9b2025b80e53b7a8fcc124947e411a'
+$exeUrl = 'https://github.com/Col-E/Recaf/releases/download/4.0.0-alpha/recaf-4x-alpha-win-86-x64.jar'
+$exeSha256 = 'a3e894306b118ef9a5e6aee4e2ba368c093320a284d933a9975c859243df81eb'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $false
