b257bd6992e9efe03cc48971860e9c9e5b664493
vm-packages <vm-packages@google.com>
2026-06-10 23:15:15 +0000

    Update upx.vm to 5.2.0

--- packages/upx.vm/tools/chocolateyinstall.ps1
+++ packages/upx.vm/tools/chocolateyinstall.ps1
@@ -4,9 +4,9 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'upx'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = "https://github.com/upx/upx/releases/download/v5.1.1/upx-5.1.1-win32.zip"
-$zipSha256 = "c0fc8553784ad64145c37a6379f7aa2daf143580437792e862ba6f4630e9f0da"
-$zipUrl_64 = 'https://github.com/upx/upx/releases/download/v5.1.1/upx-5.1.1-win64.zip'
-$zipSha256_64 = 'fa5380bca4c2718547aaa0134bc0d8a7fa27e102f0ac6371573d60d1c21d64de'
+$zipUrl = "https://github.com/upx/upx/releases/download/v5.2.0/upx-5.2.0-win32.zip"
+$zipSha256 = "28275e48a8b5ff9719c98a3d6ce68cb4437721f599ca2d904e8f4026a0a0558b"
+$zipUrl_64 = 'https://github.com/upx/upx/releases/download/v5.2.0/upx-5.2.0-win64.zip'
+$zipSha256_64 = 'b471ebf1b7f20f4a89150264ed9a008a2a5bfd247f3c6d1184a75bb59ca08f5d'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -zipUrl_64 $zipUrl_64 -zipSha256_64 $zipSha256_64 -consoleApp $true -innerFolder $true
