ee216ce4f7a177983fe77b6ab6081a862be6738e
Elliot Chernofsky <echernofsky@google.com>
2026-06-17 03:53:00 +0000

    fix memprocfs

--- packages/memprocfs.vm/tools/chocolateyinstall.ps1
+++ packages/memprocfs.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'MemProcFS'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.17/MemProcFS_files_and_binaries_v5.17.7-win_x64-20260514.zip'
-$zipSha256 = 'BC354FE023811AC6CD6DBFA8596ABFEE26FD7ED6AF82651129A16451CDB36FA7'
+$zipUrl = 'https://github.com/ufrisk/MemProcFS/releases/download/v5.17/MemProcFS_files_and_binaries_v5.17.8-win_x64-20260611.zip'
+$zipSha256 = 'd5b33be277b4fd9b04e5e66da89d74f03e2c252ceb7d4d2d10f0ac4eee3e811f'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -innerFolder $false
