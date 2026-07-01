97854b4ee76b53487cc7bfcb8d69c95eba5f849d
vm-packages <vm-packages@google.com>
2026-07-01 22:40:19 +0000

    Update cutter.vm to 2.5.0

--- packages/cutter.vm/tools/chocolateyinstall.ps1
+++ packages/cutter.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'Cutter'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.4.1/Cutter-v2.4.1-Windows-x86_64.zip'
-$zipSha256 = '7156882b79870355f25fb273f36a313b71e0e76cd97e5a5765db4d163da39175'
+$zipUrl = 'https://github.com/rizinorg/cutter/releases/download/v2.5.0/Cutter-v2.5.0-Windows-x86_64.zip'
+$zipSha256 = 'a04154a03a392dbf5886a629938582f7d23a93636fa0611c3e1c34905b197e69'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -innerFolder $true
