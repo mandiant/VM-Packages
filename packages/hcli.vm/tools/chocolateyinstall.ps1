265372e2dd292855df81f1ae514bc8c829278a5d
vm-packages <vm-packages@google.com>
2026-09-02 22:27:20 +0000

    Update hcli.vm to 0.20.1

--- packages/hcli.vm/tools/chocolateyinstall.ps1
+++ packages/hcli.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'hcli'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$exeUrl = 'https://github.com/HexRaysSA/ida-hcli/releases/download/v0.18.5/hcli-windows-x86_64-0.18.5.exe'
-$exeSha256 = 'be99bfcb9edd176e59200e6ae3df755269de2925ea53f0e48664fd67fcf28ca5'
+$exeUrl = 'https://github.com/HexRaysSA/ida-hcli/releases/download/v0.20.1/hcli-windows-x86_64-0.20.1.exe'
+$exeSha256 = '231b5482ee602312df1cd23385644eea6a677ce2f8f068b3fcefe4acd7880294'
 
 VM-Install-Single-Exe $toolName $category $exeUrl -exeSha256 $exeSha256 -consoleApp $true -arguments '--help'
