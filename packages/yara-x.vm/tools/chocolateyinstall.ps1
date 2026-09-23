2f9f2b9181828875a965711e0f280454737785d7
vm-packages <vm-packages@google.com>
2026-09-23 22:50:30 +0000

    Update yara-x.vm to 1.20.0

--- packages/yara-x.vm/tools/chocolateyinstall.ps1
+++ packages/yara-x.vm/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ Import-Module vm.common -Force -DisableNameChecking
 $toolName = 'yara-x'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
-$zipUrl = 'https://github.com/VirusTotal/yara-x/releases/download/v1.19.0/yara-x-v1.19.0-x86_64-pc-windows-msvc.zip'
-$zipSha256 = 'a68d3fcc9cb846a92a1233a6581ee21e777cd5c404d00eebf0628b25900c4f0d'
+$zipUrl = 'https://github.com/VirusTotal/yara-x/releases/download/v1.20.0/yara-x-v1.20.0-x86_64-pc-windows-msvc.zip'
+$zipSha256 = 'b1e2840bac593aea353d2b2b341f5a862c9d61c0c406d9abbbad9e1fa35163a1'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $true -executableName 'yr.exe'
