63ed829f7fbce945f63f2e057062ec65cd1ca908
vm-packages <vm-packages@google.com>
2026-09-23 22:44:35 +0000

    Fix broken hash in regcool.vm

--- packages/regcool.vm/tools/chocolateyinstall.ps1
+++ packages/regcool.vm/tools/chocolateyinstall.ps1
@@ -5,6 +5,6 @@ $toolName = 'RegCool'
 $category = VM-Get-Category($MyInvocation.MyCommand.Definition)
 
 $zipUrl = 'https://kurtzimmermann.com/files/RegCoolX64.zip'
-$zipSha256 = '2da28551cafa663a1bcab3bddaca07325485d203d193b10cf0d8036c113e0e58'
+$zipSha256 = '647f570e69cdd437a6603fe461ff98afda6f4e62d915264ad8c0f443ff1d2e34'
 
 VM-Install-From-Zip $toolName $category $zipUrl -zipSha256 $zipSha256 -consoleApp $false -innerFolder $false
